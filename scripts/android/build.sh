86be37b390fb8484137e188a26f5757ecaedc521
arturo castro <arturo@openframeworks.cc>
2016-02-11 23:53:28 +0100

    android: better logging and error catching

--- scripts/android/build.sh
+++ scripts/android/build.sh
@@ -2,7 +2,7 @@ set -ev
 # capture failing exits in commands obscured behind a pipe
 set -o pipefail
 
-APOTHECARY_PATH=$(dirname "$0")/../../apothecary
+APOTHECARY_PATH=$(realpath $(dirname "$0")/../../apothecary)
 cd $APOTHECARY_PATH
 
 # trap any script errors and exit
@@ -20,14 +20,18 @@ trapError() {
 for formula in $( ls -1 formulas | grep -v _depends) ; do
     formula_name="${formula%.*}"
     echo Compiling $formula_name
-    ./apothecary -tandroid update $formula_name > formula.log 2>&1 
+    ./apothecary -tandroid update $formula_name > $APOTHECARY_PATH/formula.log 2>&1 
+    echo $formula_name Done
 done
-cd ..
+echo Compressing libraries
+cd $APOTHECARY_PATH/..
 TARBALL=openFrameworksLibs_${TRAVIS_BRANCH}_android.tar.bz2
 tar cjf $TARBALL $(ls  | grep -v apothecary | grep -v scripts)
+echo Unencrypting key
 openssl aes-256-cbc -K $encrypted_aa785955a938_key -iv $encrypted_aa785955a938_iv -in scripts/id_rsa.enc -out scripts/id_rsa -d
 cp scripts/ssh_config ~/.ssh/config
 chmod 600 scripts/id_rsa
+echo Uploading libraries
 scp -i scripts/id_rsa $TARBALL tests@ci.openframeworks.cc:libs/$TARBALL.new
 ssh -i scripts/id_rsa tests@ci.openframeworks.cc "mv libs/$TARBALL.new libs/$TARBALL"
 rm scripts/id_rsa
