a959c197ccce45d52de1c222b69a11ceb9ced027
arturo castro <arturo@openframeworks.cc>
2016-02-15 12:39:21 +0100

    in linux64 don't pass architecture since it's already implied, OPT is used for gcc version

--- scripts/build.sh
+++ scripts/build.sh
@@ -64,7 +64,7 @@ fi
 for formula in $( ls -1 formulas | grep -v _depends) ; do
     formula_name="${formula%.*}"
     echo Compiling $formula_name
-    if [ "$OPT" != "" ]; then
+    if [ "$OPT" != "" -a "$TARGET" != "linux64" ]; then
         ./apothecary -j$PARALLEL -t$TARGET -a$OPT update $formula_name > formula.log 2>&1 &
     else
         ./apothecary -j$PARALLEL -t$TARGET update $formula_name > formula.log 2>&1 &
