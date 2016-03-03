04f5174a12dd795135be40837abc347cd8efecd2
Daniel Rosser <danoli3@gmail.com>
2016-03-03 18:07:43 +1100

    Fix linux error

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -1,3 +1,4 @@
+sudo apt-get -y update
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 100
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
