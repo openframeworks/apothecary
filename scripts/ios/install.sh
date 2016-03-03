9ec1a1b4a93ce967ffafde3c4de04e94a5c6a2c1
Daniel Rosser <danoli3@gmail.com>
2016-03-03 20:18:36 +1100

    iOS Install add Download for Pass 2

--- scripts/ios/install.sh
+++ scripts/ios/install.sh
@@ -1,1 +1,19 @@
 brew install cmake
+
+set -e
+# capture failing exits in commands obscured behind a pipe
+set -o pipefail
+
+APOTH=$(cd $(dirname "$0"); pwd -P)/../..
+APOTHECARY_PATH=$APOTH/apothecary
+cd $APOTHECARY_PATH
+
+if [ "$TARGET" == "ios" ]; then
+    if [ "$OPT2" == "1" ]; then
+        echo "Install - 1"
+    elif [ "$OPT2" == "2" ]; then
+        echo "Install - 2"
+        ./apothecary -t ios download poco
+        ./apothecary -t ios download openssl
+    fi
+fi
