80ba7479e95dc67ae20b4dcb74d9c37ee2f37202
oxillo <oxillo@hotmail.com>
2016-04-23 08:08:17 +0200

    Setup Appveyor for MSYS2 build

---
+++ scripts/msys2/install.bat
@@ -0,0 +1,22 @@
+SET MSYS2_BASE=msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz
+ECHO.Downloading %MSYS2_BASE%...
+appveyor DownloadFile http://kent.dl.sourceforge.net/project/msys2/Base/%MSYS2_ARCH%/%MSYS2_BASE%
+
+SET MSYS2_PATH=c:\msys64
+ECHO.Installing MSYS2...
+mkdir %MSYS2_PATH%
+7z x %APPVEYOR_BUILD_FOLDER%\%MSYS2_BASE% -o%MSYS2_PATH% -so | 7z x -aoa -si -ttar> nul
+ECHO.Updating MSYS2...
+(
+	echo.pacman --noconfirm -Sy pacman
+	echo.pacman --noconfirm -Syuu
+)>script.sh
+SET CHERE_INVOKING=1
+%MSYS2_PATH%\usr\bin\bash -lc "./script.sh"
+(
+	echo.pacman --noconfirm --needed -Sy make msys/unzip 
+	echo.exit
+)>script.sh
+%MSYS2_PATH%\usr\bin\bash -lc "./script.sh"
+
+%MSYS2_PATH%\autorebase.bat > nul
