fda70c50509972905a48b347296724d2376e32ab
oxillo <oxillo@hotmail.com>
2016-04-24 10:15:28 +0200

    upde zlib formula for MSYS2
    use package manager

--- apothecary/formulas/_depends/zlib.sh
+++ apothecary/formulas/_depends/zlib.sh
@@ -12,6 +12,12 @@ GIT_TAG=v$VER
 
 # download the source code and unpack it into LIB_NAME
 function download() {
+	# Skip dowloading for "MSYS2"
+	if [ "$TYPE" == "msys2" ] ; then
+		mkdir zlib #apothecary will complain about failed download if it doesn't find this directory
+		return
+	fi
+	
 	curl -LO http://zlib.net/zlib-$VER.tar.gz
 	tar -xf zlib-$VER.tar.gz
 	mv zlib-$VER zlib
@@ -35,9 +41,24 @@ function build() {
 			cmake -G "Visual Studio $VS_VER Win64"
 			vs-build "zlib.sln" Build "Release|x64"
 		fi
+	elif [ "$TYPE" == "msys2" ] ; then
+		#Install zlib with pacman if not present
+		local MSYS2_ARCH=i686
+		if [ $ARCH == 64 ] ; then 
+			MSYS2_ARCH=x86_64
+		fi
+		pkg_name=mingw-w64-${MSYS2_ARCH}-zlib
+		installed_pkg=`pacman -Q $pkg_name 2>/dev/null`
+		if [ -n "$installed_pkg" ] ; then
+			echoVerbose "package already installed : $installed_pkg"
+		else
+			echoVerbose "installing package : $pkg_name"
+			pacman --noconfirm -Sy $pkg_name
+		fi
+		return
 	else
 		./configure --static \
-					--prefix=$BUILD_ROOT_DIR \
+					--prefix=$BUILD_ROOT_DIR 
 		make clean; 
 		make
 	fi
@@ -45,7 +66,7 @@ function build() {
 
 # executed inside the lib src dir, first arg $1 is the dest libs dir root
 function copy() {
-	if [ "$TYPE" == "osx" ] ; then
+	if [ "$TYPE" == "osx" -o "$TYPE" == "msys2" ] ; then
 		return
 	elif [ "$TYPE" == "vs" ] ; then
 		if [ $ARCH == 32 ] ; then
@@ -61,6 +82,9 @@ function copy() {
 
 # executed inside the lib src dir
 function clean() {
+	if [ "$TYPE" == "osx" -o "$TYPE" == "msys2" ] ; then
+		return
+	fi
 	if [ "$TYPE" == "vs" ] ; then
 		vs-clean "zlib.sln"
 	else
