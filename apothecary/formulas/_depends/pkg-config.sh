d92fe0f67d51519bf7f091a0bcfdee4c829bb93b
oxillo <oxillo@hotmail.com>
2016-04-24 10:14:55 +0200

    update pkg-config formula for MSYS2
    use package manager

--- apothecary/formulas/_depends/pkg-config.sh
+++ apothecary/formulas/_depends/pkg-config.sh
@@ -12,6 +12,12 @@ GIT_TAG=pkg-config-$VER
 
 # download the source code and unpack it into LIB_NAME
 function download() {
+	# Skip dowload() for "msys2"
+	if [ "$TYPE" == "msys2" ] ; then
+		mkdir pkg-config #apothecary will complain about failed download if it doesn't find this directory
+		return
+	fi
+	
 	curl -LO http://pkgconfig.freedesktop.org/releases/pkg-config-$VER.tar.gz
 	tar -xf pkg-config-$VER.tar.gz
 	mv pkg-config-$VER pkg-config
@@ -20,6 +26,11 @@ function download() {
 
 # prepare the build environment, executed inside the lib src dir
 function prepare() {
+	# Skip prepare() for "msys2"
+	if [ "$TYPE" == "msys2" ] ; then
+		return
+	fi
+	
 	# generate the configure script if it's not there
 	if [ ! -f configure ] ; then
 		./autogen.sh
@@ -28,6 +39,10 @@ function prepare() {
 
 # executed inside the lib src dir
 function build() {
+	if [ "$TYPE" == "msys2" ] ; then
+		install-pkg pkg-config
+		return
+	fi
 
 	# setting empty flags so it ignores an existing pkg-config install
 	# PKG-CONFIG does not need the typical architecture flags because 
@@ -39,6 +54,10 @@ function build() {
 
 # executed inside the lib src dir, first arg $1 is the dest libs dir root
 function copy() {
+	# Skip copy() for "msys2"
+	if [ "$TYPE" == "msys2" ] ; then
+		return
+	fi
 
 	# rm link if it exists as this can cause the install to fail 
 	if [ -f $BUILD_ROOT_DIR/bin/*-pkg-config ] ; then
@@ -50,6 +69,10 @@ function copy() {
 
 # executed inside the lib src dir
 function clean() {
+	# Skip clean() for "msys2"
+	if [ "$TYPE" == "msys2" ] ; then
+		return
+	fi
 	make uninstall
 	make clean
 }
