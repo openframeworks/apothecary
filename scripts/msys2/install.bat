b1f959bddea09d1f80ff56457a5b2cd3952b8989
Olivier XILLO <oxillo@hotmail.com>
2016-04-30 19:59:47 +0200

    install

--- scripts/msys2/install.bat
+++ scripts/msys2/install.bat
@@ -1,11 +1,14 @@
-SET MSYS2_BASE=msys2-base-%MSYS2_ARCH%-%MSYS2_BASEVER%.tar.xz
+SET MSYS2_BASE=msys2-base-x86_64-%MSYS2_BASEVER%.tar.xz
 ECHO.Downloading %MSYS2_BASE%...
-appveyor DownloadFile http://kent.dl.sourceforge.net/project/msys2/Base/%MSYS2_ARCH%/%MSYS2_BASE%
+appveyor DownloadFile http://kent.dl.sourceforge.net/project/msys2/Base/x86_64/%MSYS2_BASE%
 
 SET MSYS2_PATH=c:\msys2
 ECHO.Installing MSYS2...
 mkdir %MSYS2_PATH%
-7z x %APPVEYOR_BUILD_FOLDER%\%MSYS2_BASE% -so | 7z x -aoa -si -ttar -o%MSYS2_PATH% > nul
+7z x %APPVEYOR_BUILD_FOLDER%\%MSYS2_BASE% -so | 7z x -aoa -si -ttar -oc:\install_msys2 > nul
+move c:\install_msys2 %MSYS2_PATH%
+
+%MSYS2_PATH%\autorebase.bat > nul
 dir %MSYS2_PATH%
 ECHO.Updating MSYS2...
 (
