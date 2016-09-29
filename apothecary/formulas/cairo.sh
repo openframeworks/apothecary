ec6c7bd4fc881b0247b11b48f05aeb5d4b7772f1
arturo castro <arturo@openframeworks.cc>
2016-09-29 12:52:59 +0200

    fixing paths to vs tools

--- apothecary/formulas/cairo.sh
+++ apothecary/formulas/cairo.sh
@@ -77,6 +77,7 @@ function build() {
 
 	if [ "$TYPE" == "vs" ] ; then
 		ROOT=${PWD}/..
+		export PATH="/c/Program Files (x86)/MSBuild/14.0/Bin:/C/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin:$PATH"
 		export INCLUDE="$INCLUDE;$ROOT/zlib"
 		export INCLUDE="$INCLUDE;$ROOT/libpng"
 		export INCLUDE="$INCLUDE;$ROOT/pixman/pixman"
