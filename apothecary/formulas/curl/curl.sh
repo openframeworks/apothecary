fb68256ea0c9414290b0dfae1a3a267d38ddb573
arturo castro <arturo@openframeworks.cc>
2016-10-16 13:15:14 +0200

    curl: fix 64bit detection in windows

--- apothecary/formulas/curl/curl.sh
+++ apothecary/formulas/curl/curl.sh
@@ -190,7 +190,7 @@ function copy() {
 
 cat > $1/include/curl/curlbuild.h << EOF
 /* The size of long, as computed by sizeof. */
-#ifdef __LP64__
+#if defined(__LP64__) || defined(_WIN64)
 #include "curl/curlbuild64.h"
 #else
 #include "curl/curlbuild32.h"
