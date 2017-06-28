21a03808dead07d8547be2203f19d658824ba6de
Tim Gfrerer <tim@poniesandlight.co.uk>
2017-06-28 18:14:30 +0100

    zlib: cmake - you live and learn...

--- apothecary/formulas/_depends/zlib.sh
+++ apothecary/formulas/_depends/zlib.sh
@@ -29,7 +29,7 @@ function build() {
 		unset TMP
 		unset TEMP
 		if [ $ARCH == 32 ] ; then
-			cmake -G "Visual Studio $VS_VER Win32"
+			cmake -G "Visual Studio $VS_VER"
 			vs-build "zlib.sln" Build "Release|Win32"
 		elif [ $ARCH == 64 ] ; then
 			cmake -G "Visual Studio $VS_VER Win64"
