f4257a984ea650c341202828a42693f11e70f0ec
arturo castro <arturo@openframeworks.cc>
2017-07-07 18:02:48 +0200

    linux gcc6: use ci.openframeworks.cc repo

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -69,23 +69,26 @@ if [ ! -z ${OPT+x} ]; then
         sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 100
         g++ -v
     else
-        sudo add-apt-repository -y ppa:dns/gnu
-        sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-        sudo apt-get update -q
-        sudo apt-get install gdebi
-        wget http://ci.openframeworks.cc/gcc5/gcc6debs.tar.bz2
-        tar xjf gcc6debs.tar.bz2
-        rm gcc6debs.tar.bz2
-		sudo cp *.deb /var/cache/apt/archives/
-        sudo gdebi -n gcc-6-base_6.2.0-3ubuntu11~14.04_amd64.deb
-        sudo gdebi -n cpp-6_6.2.0-3ubuntu11~14.04_amd64.deb
-        sudo gdebi -n g++-6_6.2.0-3ubuntu11~14.04_amd64.deb
-        sudo gdebi -n libstdc++-6-pic_6.2.0-3ubuntu11~14.04_amd64.deb
-        sudo gdebi -n libstdc++-6-dev_6.2.0-3ubuntu11~14.04_amd64.deb
-        sudo gdebi -n gcc-6_6.2.0-3ubuntu11~14.04_amd64.deb
-        sudo gdebi -n gcc-6-multilib_6.2.0-3ubuntu11~14.04_amd64.deb
-        sudo gdebi -n g++-6-multilib_6.2.0-3ubuntu11~14.04_amd64.deb
-        rm *.deb
+        # sudo add-apt-repository -y ppa:dns/gnu
+        # sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+        # sudo apt-get update -q
+        # sudo apt-get install gdebi
+        # wget http://ci.openframeworks.cc/gcc5/gcc6debs.tar.bz2
+        # tar xjf gcc6debs.tar.bz2
+        # rm gcc6debs.tar.bz2
+		# sudo cp *.deb /var/cache/apt/archives/
+        # sudo gdebi -n gcc-6-base_6.2.0-3ubuntu11~14.04_amd64.deb
+        # sudo gdebi -n cpp-6_6.2.0-3ubuntu11~14.04_amd64.deb
+        # sudo gdebi -n g++-6_6.2.0-3ubuntu11~14.04_amd64.deb
+        # sudo gdebi -n libstdc++-6-pic_6.2.0-3ubuntu11~14.04_amd64.deb
+        # sudo gdebi -n libstdc++-6-dev_6.2.0-3ubuntu11~14.04_amd64.deb
+        # sudo gdebi -n gcc-6_6.2.0-3ubuntu11~14.04_amd64.deb
+        # sudo gdebi -n gcc-6-multilib_6.2.0-3ubuntu11~14.04_amd64.deb
+        # sudo gdebi -n g++-6-multilib_6.2.0-3ubuntu11~14.04_amd64.deb
+        # rm *.deb
+		sudo echo deb http://ci.openframeworks.cc/gcc_repo trusty main
+		sudo apt-get update -q
+		sudo apt-get install -y gcc-6 g++-6
 
         sudo apt-get install -y gperf coreutils realpath libxrandr-dev libxinerama-dev libx11-dev libxcursor-dev libxi-dev
         sudo apt-get remove -y --purge g++-4.8
