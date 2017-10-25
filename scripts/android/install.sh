dc0bcc49ce201503991ce8d7e61483c3bfd75e87
Dan Moore <dan@makeitdoathing.com>
2017-10-25 11:12:50 -0400

    updates to test CI scripts

--- scripts/android/install.sh
+++ scripts/android/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export NDK_DIR=android-ndk-r15c
+NDK_DIR=android-ndk-r15c
 set -ev
 # capture failing exits in commands obscured behind a pipe
 set -o pipefail
@@ -10,12 +10,12 @@ if [ "$(ls -A ${NDK_DIR})" ]; then
     echo "Using cached NDK"
     ls -A ${NDK_DIR}
 else
-    echo "Downloading NDK $NDK_DIR"
-    wget "https://dl.google.com/android/repository/$NDK_DIR-linux-x86_64.zip"
+    echo "Downloading NDK"
+    wget https://dl.google.com/android/repository/${NDK_DIR}-linux-x86_64.zip
     echo "Uncompressing NDK"
-    unzip "$NDK_DIR-linux-x86_64.zip" > /dev/null 2>&1 
+    unzip ${NDK_DIR}-linux-x86_64.zip > /dev/null 2>&1 
 fi
-export NDK_ROOT=$(echo ${PWD})
+NDK_ROOT=$(echo ${PWD})
 echo "APOTHECARY DIR set to $APOTHECARY_DIR"
 echo "NDK_ROOT=${NDK_ROOT}/${NDK_DIR}" > $APOTHECARY_DIR/paths.make
 cat $APOTHECARY_DIR/paths.make
