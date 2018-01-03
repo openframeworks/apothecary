5427af568eee2a54d27e3fac85ddfb4964386dde
Jonas Jongejan <jongejan@google.com>
2018-01-03 11:58:20 -0500

    build test

--- scripts/build.sh
+++ scripts/build.sh
@@ -57,6 +57,15 @@ echoDots(){
     done
 }
 
+
+travis_fold_start() {
+  echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+travis_fold_end() {
+  echo -e "\ntravis_fold:end:$1\r"
+}
+
 if [ -z ${PARALLEL+x} ]; then
     if [ "$TARGET" == "osx" ]; then
         PARALLEL=4
@@ -89,10 +98,13 @@ if [ "$TARGET" == "emscripten" ]; then
 fi
 
 echo "Running apothecary from $PWD"
-
+ITER=0
 for formula in openssl $( ls -1 formulas | grep -v _depends | grep -v openssl | grep -v libpng | grep -v zlib | grep -v libxml2 ) ; do
     start=`date +%s`
     formula_name="${formula%.*}"
+
+    travis_fold_start "build.$ITER" "Build $formula_name"
+
     if [ "$OPT" != "" -a "$TARGET" != "linux64" ]; then
         echo Compiling $formula_name
         echo "./apothecary -f -j$PARALLEL -t$TARGET -a$OPT update $formula_name" > formula.log 2>&1
@@ -145,6 +157,9 @@ for formula in openssl $( ls -1 formulas | grep -v _depends | grep -v openssl | 
     end=`date +%s`
     runtime=$((end-start))
     echo "Finished $formula_name in $runtime sec"
+    travis_fold_end "build.$ITER"
+
+    ITER=$(expr $ITER + 1)
 done
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]] || [ ! -z ${APPVEYOR+x} ]; then
