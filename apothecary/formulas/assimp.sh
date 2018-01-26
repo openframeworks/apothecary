073d19fd5fbff0d597692f527e4acc9570f710fd
Luis Martinell Andreu <luis@catchoom.com>
2018-01-26 19:18:04 +0100

    Fix assimp build for arm64

--- apothecary/formulas/assimp.sh
+++ apothecary/formulas/assimp.sh
@@ -146,7 +146,7 @@ function build() {
 
     elif [ "$TYPE" == "android" ] ; then
 
-        source ../../android_configure.sh $ABI
+        #source ../../android_configure.sh $ABI
 
 
         if [ "$ABI" == "armeabi-v7a" ]; then
@@ -186,6 +186,7 @@ function build() {
                 -DCMAKE_INSTALL_PREFIX=install"
         elif [ "$ABI" == "x86" ]; then
             export HOST=x86-linux-android
+
             local buildOpts="--build build/$TYPE
                 -DBUILD_SHARED_LIBS=OFF
                 -DASSIMP_BUILD_STATIC_LIB=1
@@ -205,10 +206,36 @@ function build() {
 
         # -DASSIMP_BUILD_STL_IMPORTER=0
         # -DASSIMP_BUILD_BLEND_IMPORTER=0
+        
+        elif [ "$ARCH" == "arm64" ]; then
+            export HOST=aarch-linux-android
+        fi
+ # -- Enabled formats: AMF 3DS AC ASE ASSBIN ASSXML B3D BVH COLLADA DXF CSM HMP IRRMESH IRR LWO LWS MD2 MD3 MD5 MDC MDL NFF NDO OFF OBJ OGRE OPENGEX PLY MS3D COB BLEND IFC XGL FBX Q3D Q3BSP RAW SIB SMD TERRAGEN 3D X X3D GLTF 3MF MMD
+
+        local buildOpts="
+            -DBUILD_SHARED_LIBS=OFF 
+            -DASSIMP_BUILD_STATIC_LIB=1
+            -DASSIMP_BUILD_TESTS=0
+            -DASSIMP_BUILD_SAMPLES=0
+            -DASSIMP_BUILD_STL_IMPORTER=0
+            -DASSIMP_BUILD_BLEND_IMPORTER=0
+            -DASSIMP_BUILD_3MF_IMPORTER=0
+            -DASSIMP_ENABLE_BOOST_WORKAROUND=1
+            -DANDROID_NDK=$NDK_ROOT 
+            -DCMAKE_BUILD_TYPE=Release
+            -DANDROID_ABI=$ABI
+            -DANDROID_STL=c++_static
+            -DANDROID_NATIVE_API_LEVEL=$ANDROID_PLATFORM
+            -DANDROID_FORCE_ARM_BUILD=TRUE
+            -DCMAKE_INSTALL_PREFIX=install"
+
+        CFLAGS=""
+        CXXFLAGS=""
+        LDFLAGS=""
         rm -rf build_$ABI
         mkdir -p build_$ABI
         cd build_$ABI
-        cmake -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE="${NDK_ROOT}/build/cmake/android.toolchain.cmake" $buildOpts -DCMAKE_C_FLAGS="-O3 -DNDEBUG -fsigned-char ${CFLAGS}" -DCMAKE_CXX_FLAGS="-O3 -DNDEBUG -fsigned-char ${CFLAGS}" -DCMAKE_LD_FLAGS="$LDFLAGS" ..
+        cmake -G 'Unix Makefiles' -DCMAKE_TOOLCHAIN_FILE="${NDK_ROOT}/build/cmake/android.toolchain.cmake" $buildOpts -DCMAKE_C_FLAGS="-O3 -DNDEBUG -fsigned-char ${CFLAGS}" -DCMAKE_CXX_FLAGS="-O3 -DNDEBUG -fsigned-char ${CFLAGS}" -DCMAKE_LD_FLAGS="$LDFLAGS " ..
         make assimp -j${PARALLEL_MAKE}
         cd ..
 
