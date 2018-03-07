7225ca22f328d04fa10093ccc23f20e66cea434c
arturo castro <arturo@openframeworks.cc>
2018-03-07 18:15:13 +0100

    poco: trying to update to latest release

--- apothecary/formulas/poco/poco.sh
+++ apothecary/formulas/poco/poco.sh
@@ -8,11 +8,11 @@
 # specify specfic build configs in poco/config using ./configure --config=NAME
 
 # define the version
-VER=1.7.2-release
+VER=1.8.1-release
 
 # tools for git use
 GIT_URL=https://github.com/pocoproject/poco
-GIT_TAG=poco-1.7.2-release
+GIT_TAG=poco-1.8.1-release
 
 FORMULA_TYPES=( "osx" "ios" "tvos" "android" "emscripten" "vs" "linux" "linux64" "linuxarmv6l")
 
