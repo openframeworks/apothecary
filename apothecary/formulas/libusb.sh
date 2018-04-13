b90b257f514b39927fccf64a289bc6b634809773
arturo castro <arturo@openframeworks.cc>
2018-04-13 14:53:53 +0200

    libusb: fixes for osx

--- apothecary/formulas/libusb.sh
+++ apothecary/formulas/libusb.sh
@@ -32,8 +32,8 @@ function download() {
 # prepare the build environment, executed inside the lib src dir
 function prepare() {
 	if [ "$TYPE" == "osx" ] ; then
-		sed -i -e "s/libusb-1\.0\.0\.dylib/libusb-1\.0\.0\.a/g" Xcode/libusb.xcodeproj/project.pbxproj
-		sed -i -e "s/productType = \"com\.apple\.product-type\.library\.dynamic\";/productType = \"com\.apple\.product-type\.library\.static\";/g"
+		sed -i -e 's/libusb-1\.0\.0\.dylib/libusb-1\.0\.0\.a/g' Xcode/libusb.xcodeproj/project.pbxproj
+		sed -i -e 's/productType = "com\.apple\.product-type\.library\.dynamic";/productType = "com\.apple\.product-type\.library\.static";/g'  Xcode/libusb.xcodeproj/project.pbxproj
 	fi
 }
 
