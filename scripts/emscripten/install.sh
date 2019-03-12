74917dcb6e3a6308415e4bab8d23df4f573bd19d
Arturo Castro <arturo@rin.rs>
2019-03-12 19:39:18 +0100

    revert emscripten changes

--- scripts/emscripten/install.sh
+++ scripts/emscripten/install.sh
@@ -19,13 +19,4 @@ sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
 cd ~/
 git clone --depth 1 --single-branch --branch master https://github.com/urho3d/emscripten-sdk
 cd emscripten-sdk
-# ./emsdk activate --build=Release sdk-master-64bit
-./emsdk update-tags
-./emsdk install sdk-1.38.28-64bit
-./emsdk activate sdk-1.38.28-64bit
-
-# cd ~/
-# git clone --depth=1 --single-branch --branch master https://github.com/emscripten-core/emsdk.git emscripten-sdk
-# cd emscripten-sdk
-# ./emsdk install sdk-1.38.28-64bit
-# ./emsdk activate sdk-1.38.28-64bit
+./emsdk activate --build=Release sdk-master-64bit
