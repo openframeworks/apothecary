ee1bf656aa76fb3448ac1f15e05407927b02a736
Arturo Castro <arturo@rin.rs>
2019-03-12 19:18:12 +0100

    fixing emscripten

--- scripts/emscripten/install.sh
+++ scripts/emscripten/install.sh
@@ -14,15 +14,18 @@ trapError() {
 
 cd $TRAVIS_BUILD_DIR
 
-# cp scripts/emscripten/.emscripten ~/
-# sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
-# cd ~/
-# git clone --depth 1 --single-branch --branch master https://github.com/urho3d/emscripten-sdk
-# cd emscripten-sdk
-# ./emsdk activate --build=Release sdk-master-64bit
-
+cp scripts/emscripten/.emscripten ~/
+sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
 cd ~/
-git clone --depth=1 --single-branch --branch master https://github.com/emscripten-core/emsdk.git emscripten-sdk
+git clone --depth 1 --single-branch --branch master https://github.com/urho3d/emscripten-sdk
 cd emscripten-sdk
+# ./emsdk activate --build=Release sdk-master-64bit
+./emsdk update
 ./emsdk install sdk-1.38.28-64bit
 ./emsdk activate sdk-1.38.28-64bit
+
+# cd ~/
+# git clone --depth=1 --single-branch --branch master https://github.com/emscripten-core/emsdk.git emscripten-sdk
+# cd emscripten-sdk
+# ./emsdk install sdk-1.38.28-64bit
+# ./emsdk activate sdk-1.38.28-64bit
