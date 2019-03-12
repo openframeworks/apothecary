3cb9b14690d96b107d0bc65fc8e41cf7ca2b2e2b
Arturo Castro <arturo@rin.rs>
2019-03-12 17:37:08 +0100

    emscripten: install latest toolchain instead of prebuilt
    prebuilt seems not maintained anymore

--- scripts/emscripten/install.sh
+++ scripts/emscripten/install.sh
@@ -14,9 +14,15 @@ trapError() {
 
 cd $TRAVIS_BUILD_DIR
 
-cp scripts/emscripten/.emscripten ~/
-sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
-cd ~/
-git clone https://github.com/urho3d/emscripten-sdk
+# cp scripts/emscripten/.emscripten ~/
+# sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
+# cd ~/
+# git clone https://github.com/urho3d/emscripten-sdk
+# cd emscripten-sdk
+# ./emsdk activate latest
+
+git clone --depth=1 https://github.com/emscripten-core/emsdk.git emscripten-sdk
 cd emscripten-sdk
+./emsdk install latest
 ./emsdk activate latest
+source ./emsdk_env.sh
