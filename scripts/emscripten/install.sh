8a47e3608b294ac8770701a755e197d7367e8b26
Arturo Castro <arturo@rin.rs>
2019-03-15 19:08:26 +0100

    emscripten: trying travis official docker emscripten image

--- scripts/emscripten/install.sh
+++ scripts/emscripten/install.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+
 #set -e
 #set -o pipefail
+
 # trap any script errors and exit
-trap "trapError" ERR
+# trap "trapError" ERR
 
-SUDO=
+# SUDO=
 
-trapError() {
-	echo
-	echo " ^ Received error ^"
-	exit 1
-}
+# trapError() {
+# 	echo
+# 	echo " ^ Received error ^"
+# 	exit 1
+# }
 
-cd $TRAVIS_BUILD_DIR
+# cd $TRAVIS_BUILD_DIR
 
-cp scripts/emscripten/.emscripten ~/
-sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
-cd ~/
-git clone --depth 1 --single-branch --branch master https://github.com/urho3d/emscripten-sdk
-cd emscripten-sdk
-./emsdk activate --build=Release sdk-master-64bit
+# cp scripts/emscripten/.emscripten ~/
+# sed -i "s|%HOME%|${HOME}|g" ~/.emscripten
+# cd ~/
+# git clone --depth 1 --single-branch --branch master https://github.com/urho3d/emscripten-sdk
+# cd emscripten-sdk
+# ./emsdk activate --build=Release sdk-master-64bit
