0258226f5d7a2d3ef0ea68f3485c37e75f716d90
Arturo Castro <arturo@rin.rs>
2019-03-15 19:27:43 +0100

    fixing emscripten

--- scripts/emscripten/install.sh
+++ scripts/emscripten/install.sh
@@ -23,5 +23,8 @@
 # cd emscripten-sdk
 # ./emsdk activate --build=Release sdk-master-64bit
 
-docker exec -it emscripten sudo touch /usr/bin/emcmake;
+docker exec -it emscripten sudo touch echo <<< EOF;
+#!/usr/bin/env bash
+$@
+EOF > /usr/bin/emcmake
 docker exec -it emscripten sudo chmod 755 /usr/bin/emcmake;
