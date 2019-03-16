5104438454f9d89d95b60c11d6a1f9e054acc495
Arturo Castro <arturo@rin.rs>
2019-03-16 13:17:38 +0100

    fixing emscripten

---
+++ scripts/build_one.sh
@@ -0,0 +1,25 @@
+trap "trapError" ERR
+
+echo Build $formula_name
+
+local ARGS="-f -j$PARALLEL -p -t$TARGET -d$OUTPUT_FOLDER "
+if [ "$ARCH" != "" ] ; then
+    ARGS="$ARGS -a$ARCH"
+fi
+
+if [ "$VERBOSE" = true ] ; then
+    echo "./apothecary $ARGS update $formula_name"
+    cd $APOTHECARY_PATH
+    ./apothecary $ARGS update $formula_name
+else
+    echo "./apothecary $ARGS update $formula_name" > formula.log 2>&1
+    cd $APOTHECARY_PATH
+    ./apothecary $ARGS update $formula_name >> formula.log 2>&1 &
+
+    apothecaryPID=$!
+    echoDots $apothecaryPID
+    wait $apothecaryPID
+
+    echo "Tail of log for $formula_name"
+    tail -n 100 formula.log
+fi
