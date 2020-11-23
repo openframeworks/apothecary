e5b2e63cc43f3922dcfdf65cb3d711cce62a6a1c
ofTheo <theo@openframeworks.cc>
2020-11-23 14:15:13 -0800

    attempt to switch the sdk path in install instead

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -4,3 +4,4 @@ brew update >/dev/null
 brew install cmake coreutils autoconf automake ccache
 brew reinstall libtool
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
+xcode-select -switch /Applications/Xcode-12.2.app
