0876f2b11894755a229c23d24d5b551f3c6886af
ofTheo <theo@openframeworks.cc>
2020-11-24 11:01:30 -0800

    another sanity check

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -5,5 +5,6 @@ brew install cmake coreutils autoconf automake ccache
 brew reinstall libtool
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
 export DEVELOPER_DIR="/Applications/Xcode-12.2.app/Contents/Developer"
-echo "sdk path is $(xcode-select -p)"
-echo "ld path is $(xcrun -f ld)"
+echo "xcode select sdk path is $(xcode-select -p)"
+echo "xcrun find ld path is $(xcrun -f ld)"
+echo "xcrun show sdk path is $(xcrun --show-sdk-path)"
