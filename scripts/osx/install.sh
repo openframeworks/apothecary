c1ec450db11dd7c2dcd8f1c7675f4fa41d9269d8
ofTheo <theo@openframeworks.cc>
2020-11-24 11:56:21 -0800

    hoppefully this will set the proper path

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -5,6 +5,7 @@ brew install cmake coreutils autoconf automake ccache
 brew reinstall libtool
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
 export DEVELOPER_DIR="/Applications/Xcode-12.2.app/Contents/Developer"
-echo "xcode select sdk path is $(xcode-select -p)"
-echo "xcrun find ld path is $(xcrun -f ld)"
-echo "xcrun show sdk path is $(xcrun --show-sdk-path)"
+export SDKROOT="/Applications/Xcode-12.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk"
+echo "xcode-select -p       is $(xcode-select -p)"
+echo "xcrun -f ld           is $(xcrun -f ld)"
+echo "xcrun --show-sdk-path is $(xcrun --show-sdk-path)"
