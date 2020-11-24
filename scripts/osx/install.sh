2b51bfc9f17783aa105a1c149f8df7bea8243b83
ofTheo <theo@openframeworks.cc>
2020-11-24 10:19:43 -0800

    trying to fix travis broken default xcode path

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -4,3 +4,6 @@ brew update >/dev/null
 brew install cmake coreutils autoconf automake ccache
 brew reinstall libtool
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
+export DEVELOPER_DIR="/Applications/Xcode-12.2.app/Contents/Developer"
+echo "sdk path is $(xcode-select -p)"
+echo "ld path is $(xcrun -f ld)"
