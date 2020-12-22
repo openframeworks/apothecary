686aeaf881c98495428b89773f36f726766a6a9a
ofTheo <theo@openframeworks.cc>
2020-12-22 09:37:20 -0800

    debugging

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -4,6 +4,9 @@ brew update >/dev/null
 brew install cmake coreutils autoconf automake ccache
 brew reinstall libtool
 
-sudo xcode-select -switch "/Applications/Xcode_12.2.app/Contents/Developer"
+sudo xcode-select -switch $DEVELOPER_DIR
+
+local SDK_PATH=$(xcrun --sdk macosx --show-sdk-path)
+echo "SDK PATH IS ${SDK_PATH}"
 
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
