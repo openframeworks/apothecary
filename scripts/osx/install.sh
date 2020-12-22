c52df136738170e9311eab20e9a673a5c7509037
ofTheo <theo@openframeworks.cc>
2020-12-22 09:33:51 -0800

    try and set the dev directory

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -4,4 +4,6 @@ brew update >/dev/null
 brew install cmake coreutils autoconf automake ccache
 brew reinstall libtool
 
+sudo xcode-select -switch "/Applications/Xcode_12.2.app/Contents/Developer"
+
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
