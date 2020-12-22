aef8223399cfac9e7fc51660f04b05011ed2c6fc
ofTheo <theo@openframeworks.cc>
2020-12-22 08:28:32 -0800

    same fix as travis for macos

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -3,4 +3,7 @@ brew update >/dev/null
 
 brew install cmake coreutils autoconf automake ccache
 brew reinstall libtool
+
+export DEVELOPER_DIR="/Applications/Xcode_12.2.app/Contents/Developer"
+
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
