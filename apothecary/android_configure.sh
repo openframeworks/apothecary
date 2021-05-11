f2b7c2ae6be742ec763ee09a521a154a4fc68234
Dan Rosser <danoli3@gmail.com>
2021-05-11 18:08:17 +1000

    Android Configure Fixes

--- apothecary/android_configure.sh
+++ apothecary/android_configure.sh
@@ -2,10 +2,13 @@
 export ABI=$1
 if [ "$(uname)" = "Darwin" ]; then
     export HOST_PLATFORM=darwin-x86_64
+    export ANDROID_TOOLHOST="linux-android"
 elif [ "$(uname)" == "windows" ]; then
-        ANDROID_HOST="windows-x86_64"
+    export ANDROID_HOST="windows-x86_64"
+    export ANDROID_TOOLHOST="windows-android"
 else
     export HOST_PLATFORM=linux-x86_64
+    export ANDROID_TOOLHOST="linux-android"
 fi
 export LIBSPATH=android/$ABI
 export NDK_PLATFORM=$ANDROID_PLATFORM
@@ -13,23 +16,28 @@ export TOOLCHAIN_VERSION=4.9
 export CLANG_VERSION=
 if [ "$ABI" = "armeabi-v7a" ] || [ "$ABI" = "armeabi" ]; then
     export SYSROOT="${NDK_ROOT}/platforms/$ANDROID_PLATFORM/arch-arm"
-    export ANDROID_PREFIX=arm-linux-androideabi
+    export ANDROID_PREFIX=arm-${ANDROID_TOOLHOST}eabi
+    export ANDROID_POSTFIX=${ANDROID_PREFIX}
     export GCC_TOOLCHAIN=$ANDROID_PREFIX-${TOOLCHAIN_VERSION}
 elif [ "$ABI" = "arm64-v8a" ]; then
     export SYSROOT="${NDK_ROOT}/platforms/$ANDROID_PLATFORM/arch-arm64"
-    export ANDROID_PREFIX=aarch64-linux-android
+    export ANDROID_PREFIX=aarch64-${ANDROID_TOOLHOST}
+    export ANDROID_POSTFIX=${ANDROID_PREFIX}
     export GCC_TOOLCHAIN=$ANDROID_PREFIX-${TOOLCHAIN_VERSION}
 elif [ "$ABI" = "x86" ]; then
     export SYSROOT="${NDK_ROOT}/platforms/$ANDROID_PLATFORM/arch-x86"
-    export ANDROID_PREFIX=x86-linux-android
+    export ANDROID_PREFIX=x86-${ANDROID_TOOLHOST}
+    export ANDROID_POSTFIX=${ANDROID_PREFIX}
     export GCC_TOOLCHAIN=x86-${TOOLCHAIN_VERSION}
 elif [ "$ABI" = "x86_64" ]; then
     export SYSROOT="${NDK_ROOT}/platforms/$ANDROID_PLATFORM/arch-x86_64"
     export ANDROID_PREFIX=x86_64
-    export GCC_TOOLCHAIN=x86-${TOOLCHAIN_VERSION}
+    export ANDROID_POSTFIX=x86_64-${ANDROID_TOOLHOST}
+    export GCC_TOOLCHAIN=x86_64-${TOOLCHAIN_VERSION}
 elif [ $ABI = arm64-v8a ]; then
     export SYSROOT=${NDK_ROOT}/sysroot
-    export ANDROID_PREFIX=aarch64-linux-android
+    export ANDROID_PREFIX=aarch64-${ANDROID_TOOLHOST}
+    export ANDROID_POSTFIX=${ANDROID_PREFIX}
     export GCC_TOOLCHAIN=$ANDROID_PREFIX-${TOOLCHAIN_VERSION}
 fi
 export ANDROID_CMAKE_TOOLCHAIN=${NDK_ROOT}/build/cmake/android.toolchain.cmake
@@ -39,28 +47,28 @@ export GCC_TOOLCHAIN_PATH=${NDK_ROOT}/toolchains/${GCC_TOOLCHAIN}/prebuilt/${HOS
 export PATH=${PATH}:${TOOLCHAIN_PATH}
 export CC=${TOOLCHAIN_PATH}/clang
 export CXX=${TOOLCHAIN_PATH}/clang++
-export AR=${NDK_ROOT}/toolchains/${ANDROID_PREFIX}-${TOOLCHAIN_VERSION}/prebuilt/${HOST_PLATFORM}/${ANDROID_PREFIX}/bin/ar
-export RANLIB=${NDK_ROOT}/toolchains/${ANDROID_PREFIX}-${TOOLCHAIN_VERSION}/prebuilt/${HOST_PLATFORM}/${ANDROID_PREFIX}/bin/ranlib
+export AR=${NDK_ROOT}/toolchains/${ANDROID_PREFIX}-${TOOLCHAIN_VERSION}/prebuilt/${HOST_PLATFORM}/${ANDROID_POSTFIX}/bin/ar
+export RANLIB=${NDK_ROOT}/toolchains/${ANDROID_PREFIX}-${TOOLCHAIN_VERSION}/prebuilt/${HOST_PLATFORM}/${ANDROID_POSTFIX}/bin/ranlib
 export CFLAGS="-nostdlib --sysroot=${SYSROOT} -fno-short-enums -fPIE -fPIC"
-export CFLAGS="$CFLAGS -I${SYSROOT}/usr/include/ -I${SYSROOT}/usr/include/${ANDROID_PREFIX} -I${NDK_ROOT}/sources/android/support/include -I${NDK_ROOT}/sources/cxx-stl/llvm-libc++/include -I${NDK_ROOT}/sources/android/cpufeatures "
+export CFLAGS="$CFLAGS -I${SYSROOT}/usr/include/ -I${SYSROOT}/usr/include/${ANDROID_POSTFIX} -I${NDK_ROOT}/sources/android/support/include -I${NDK_ROOT}/sources/cxx-stl/llvm-libc++/include -I${NDK_ROOT}/sources/android/cpufeatures "
 export LDFLAGS="-pie -nostdlib -L${NDK_ROOT}/sources/cxx-stl/llvm-libc++/libs/${ABI} -lz -llog -std=c++17 -stdlib=libc++ -lgcc -lc -lm -ldl" #-lc++ -lc++abi -lunwind
-export LIBS="-lz -llog  -std=c++17 -stdlib=libc++  -lgcc -lc -lm -ldl"
+export LIBS="-lz -llog  -std=c++17 -stdlib=libc++  \-lgcc -lc -lm -ldl"
 # -ldl -lm -lc "
 #export ANDROID_SYSROOT=${SYSROOT}
 
 if [ "$ABI" = "armeabi-v7a" ]; then
-    export CFLAGS="$CFLAGS -target armv7-none-linux-androideabi -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
+    export CFLAGS="$CFLAGS -target armv7-none-linux-androideabi -march=armv7-a -mfloat-abi=softfp -mfpu=neon -isystem -I${NDK_ROOT}/sysroot/usr/include/arm-linux-androideabi"
     export LDFLAGS="$LDFLAGS -target armv7-none-linux-androideabi -march=armv7-a -mfloat-abi=softfp -mfpu=neon -Wl,--fix-cortex-a8 -Wl,--no-undefined"
 elif [ "$ABI" = "armeabi" ]; then
-    export CFLAGS="$CFLAGS"
+    export CFLAGS="$CFLAGS -isystem -I${NDK_ROOT}/sysroot/usr/include/arm-linux-androideabi"
     export LDFLAGS="$LDFLAGS -Wl,--fix-cortex-a8 -shared -Wl,--no-undefined"
 elif [ $ABI = "arm64-v8a" ]; then
-    export CFLAGS="$CFLAGS -target aarch64-linux-android -mfpu=neon"
+    export CFLAGS="$CFLAGS -target aarch64-linux-android -mfpu=neon -isystem -I${NDK_ROOT}/sysroot/usr/include/aarch64-linux-android"
     export LDFLAGS="$LDFLAGS -target aarch64-linux-android -mfpu=neon"
 elif [ "$ABI" = "x86_64" ]; then
-    export CFLAGS="$CFLAGS -target x86_64-linux-android"
+    export CFLAGS="$CFLAGS -target x86_64-linux-android -I${NDK_ROOT}/sysroot/usr/include/x86_64-linux-android"
     export LDFLAGS="$LDFLAGS -target x86_64-linux-android -Wl,--fix-cortex-a8 -shared -Wl,--no-undefined"
 elif [ "$ABI" = "x86" ]; then
-    export CFLAGS="$CFLAGS -target i686-none-linux-android -march=i686 -msse3 -mstackrealign -mfpmath=sse -fno-stack-protector"
+    export CFLAGS="$CFLAGS -target i686-none-linux-android -march=i686 -msse3 -mstackrealign -mfpmath=sse -fno-stack-protector -isystem -I${NDK_ROOT}/sysroot/usr/include/i686-linux-android" 
     export LDFLAGS="$LDFLAGS -target i686-none-linux-android -march=i686"
 fi
