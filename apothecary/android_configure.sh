30c43925f696b9cb8c4c3b56919aaaa2187733d4
Dan Rosser <danoli3@gmail.com>
2021-07-09 19:05:06 +1000

    Android Fix Missing include for wchar

--- apothecary/android_configure.sh
+++ apothecary/android_configure.sh
@@ -41,7 +41,7 @@ elif [ "$ABI" = "x86" ]; then
     export ANDROID_POSTFIX=i686-${ANDROID_TOOLHOST}
     export GCC_TOOLCHAIN=x86-${TOOLCHAIN_VERSION}
     export PLATFORM_LIBS=$LIB_SYSROOT/usr/lib
-elif [ "$ABI" = "x86-64" ]; then
+elif [ "$ABI" = "x86_64" ]; then
     export SYSROOT="${NDK_ROOT}/sysroot"
     export LIB_SYSROOT="${NDK_ROOT}/platforms/$ANDROID_PLATFORM/arch-x86_64"
     export ANDROID_PREFIX=x86_64
@@ -55,13 +55,14 @@ export TOOLCHAIN_PATH=${NDK_ROOT}/toolchains/${TOOLCHAIN}/prebuilt/${HOST_PLATFO
 export DEEP_TOOLCHAIN_PATH=${NDK_ROOT}/toolchains/${TOOLCHAIN}/prebuilt/${HOST_PLATFORM}/sysroot/usr/lib/$ANDROID_POSTFIX/$ANDROID_API
 export GCC_TOOLCHAIN_PATH=${NDK_ROOT}/toolchains/${GCC_TOOLCHAIN}/prebuilt/${HOST_PLATFORM}
 export TOOLCHAIN_INCLUDE_PATH=${NDK_ROOT}/toolchains/${TOOLCHAIN}/prebuilt/${HOST_PLATFORM}/sysroot/usr/include
+export TOOLCHAIN_LOCAL_INCLUDE_PATH=${NDK_ROOT}/toolchains/${TOOLCHAIN}/prebuilt/${HOST_PLATFORM}/sysroot/usr/local/include
 export PATH=${PATH}:${TOOLCHAIN_PATH}
 export CC=${TOOLCHAIN_PATH}/clang
 export CXX=${TOOLCHAIN_PATH}/clang++
 export AR=${NDK_ROOT}/toolchains/${ANDROID_PREFIX}-${TOOLCHAIN_VERSION}/prebuilt/${HOST_PLATFORM}/${ANDROID_POSTFIX}/bin/ar
 export RANLIB=${NDK_ROOT}/toolchains/${ANDROID_PREFIX}-${TOOLCHAIN_VERSION}/prebuilt/${HOST_PLATFORM}/${ANDROID_POSTFIX}/bin/ranlib
-export CFLAGS="-std=c17 --sysroot=${LIB_SYSROOT} -I${SYSROOT}/usr/include/${ANDROID_POSTFIX} -fno-short-enums -fPIE -fPIC -fuse-ld=gold"
-export CPPFLAGS="-stdlib=libc++ -I${SYSROOT}/usr/include/ -I${SYSROOT}/usr/include/${ANDROID_POSTFIX} -I${NDK_ROOT}/sources/android/support/include -I${NDK_ROOT}/sources/cxx-stl/llvm-libc++/include -I${NDK_ROOT}/sources/android/cpufeatures -I${TOOLCHAIN_INCLUDE_PATH} -I${TOOLCHAIN_INCLUDE_PATH}/${ANDROID_POSTFIX}"  #-DANDROID_STL=c++_static -
+export CFLAGS="-std=c17 --sysroot=${LIB_SYSROOT} -I${SYSROOT}/usr/include/${ANDROID_POSTFIX} -fno-short-enums -fPIE -fPIC -fuse-ld=gold -I${NDK_ROOT}/sources/android/cpufeatures -I${TOOLCHAIN_INCLUDE_PATH} -I${TOOLCHAIN_INCLUDE_PATH}/${ANDROID_POSTFIX} -I${TOOLCHAIN_LOCAL_INCLUDE_PATH}"
+export CPPFLAGS="-stdlib=libc++ -I${SYSROOT}/usr/include/ -I${SYSROOT}/usr/include/${ANDROID_POSTFIX} -I${NDK_ROOT}/sources/android/support/include -I${NDK_ROOT}/sources/cxx-stl/llvm-libc++/include -I${NDK_ROOT}/sources/android/cpufeatures -I${TOOLCHAIN_INCLUDE_PATH} -I${TOOLCHAIN_INCLUDE_PATH}/${ANDROID_POSTFIX} -I${TOOLCHAIN_LOCAL_INCLUDE_PATH}"  #-DANDROID_STL=c++_static -
 export CXXFLAGS="-std=c++17 -stdlib=libc++ --sysroot=${LIB_SYSROOT} -fno-short-enums -fPIE -fPIC -fuse-ld=gold"
 
 export LDFLAGS="-pie -L${LIB_SYSROOT}/usr/lib -L${NDK_ROOT}/sources/cxx-stl/llvm-libc++/libs/${ABI} -L$PLATFORM_LIBS -L$DEEP_TOOLCHAIN_PATH" #-lc++ -lc++abi -lunwind
@@ -69,7 +70,7 @@ export LIBS="-lz -lgcc -lc -lm -ldl"
 # -ldl -lm -lc "
 #export ANDROID_SYSROOT=${SYSROOT}
 
-echo "Toolchain: ${TOOLCHAIN_INCLUDE_PATH}/${ANDROID_POSTFIX}"
+echo "Toolchain: ${TOOLCHAIN_LOCAL_INCLUDE_PATH}"
 if [ "$ABI" = "armeabi-v7a" ]; then
     export CFLAGS="$CFLAGS -target armv7-none-linux-androideabi -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
     export CPPFLAGS="$CPPFLAGS -I${NDK_ROOT}/sysroot/usr/include/arm-linux-androideabi -target armv7-none-linux-androideabi -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
