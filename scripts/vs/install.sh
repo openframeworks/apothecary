3416925ea17f22e8dc8ba15edd49c071b2c9bb0d
ofTheo <theo@openframeworks.cc>
2022-02-07 18:42:14 -0800

    fix7

--- scripts/vs/install.sh
+++ scripts/vs/install.sh
@@ -1,5 +1,5 @@
-$MSYS2_PATH/usr/bin/pacman --noconfirm --needed -Su
+c:/msys64/usr/bin/pacman --noconfirm --needed -Su
 
 #choco install -y strawberryperl
 
-PATH=$PATH;$MSYS2_PATH/usr/bin;
+PATH=$PATH;c:/msys64/usr/bin;
