a22cf8731458f6e481a89d3e206b16df26a473a4
Dan Rosser <danoli3@gmail.com>
2022-03-29 21:17:04 +1100

    git to https: protocol

--- scripts/linuxaarch64/install.sh
+++ scripts/linuxaarch64/install.sh
@@ -91,7 +91,7 @@ EOF
 }
 
 installJunest(){
-	git clone git://github.com/fsquillace/junest ~/.local/share/junest
+	git clone https://github.com/fsquillace/junest ~/.local/share/junest
 	export PATH=~/.local/share/junest/bin:$PATH
     junest setup
     junest -- << EOF
