50870946c674a743b99ab458437ed5ac38f81854
Dimitre <dimitre@users.noreply.github.com>
2022-04-05 16:19:50 -0300

    adding wget, needed in macOs

--- scripts/osx/install.sh
+++ scripts/osx/install.sh
@@ -1,7 +1,7 @@
 # Silent update to prevent long logs
 brew update >/dev/null
 
-brew install cmake coreutils autoconf automake ccache
+brew install cmake coreutils autoconf automake ccache wget
 # brew reinstall libtool
 
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
