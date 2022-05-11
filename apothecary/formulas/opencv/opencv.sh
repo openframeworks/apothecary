5658689d4011245c8439c64cf0c29c2372b48b5b
ofTheo <theo@openframeworks.cc>
2022-05-11 09:09:13 -0700

    blah

--- apothecary/formulas/opencv/opencv.sh
+++ apothecary/formulas/opencv/opencv.sh
@@ -227,8 +227,8 @@ function build() {
       -DWITH_PNG=OFF \
       -DWITH_OPENCL=OFF \
       -DWITH_PVAPI=OFF  | tee ${LOG}
-      vs-build "OpenCV.sln"
-      #vs-build "OpenCV.sln" Build "Debug|x64"
+      vs-build "OpenCV.sln" Build "Release|x64"
+      vs-build "OpenCV.sln" Build "Debug|x64"
     fi
 
   elif [[ "$TYPE" == "ios" || "${TYPE}" == "tvos" ]] ; then
