b94e52f183749ba2846bd65ea65cbc7ecbfa72ca
ofTheo <theo@openframeworks.cc>
2022-05-12 15:43:32 -0700

    everything but android curl

--- apothecary/formulas/curl/curl.sh
+++ apothecary/formulas/curl/curl.sh
@@ -66,7 +66,7 @@ function build() {
 
 	elif [ "$TYPE" == "android" ]; then
 	    local BUILD_TO_DIR=$BUILD_DIR/curl/build/$TYPE/$ABI
-        local OPENSSL_DIR=$BUILD_DIR/openssl/build/
+        local OPENSSL_DIR=$BUILD_DIR/openssl/
 	    source ../../android_configure.sh $ABI
 
 	    if [ "$ARCH" == "armv7" ]; then
