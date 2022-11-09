a4c4d6de67b5cbc47df57031d715c2685bc257b0
themancalledjakob <jakob@jrkb.land>
2022-11-09 11:45:49 +0100

    compile with c++11
    this fixes various issues of deprecated/removed semantics and types if compiled with c++17

--- apothecary/formulas/FreeImage/FreeImage.sh
+++ apothecary/formulas/FreeImage/FreeImage.sh
@@ -333,6 +333,7 @@ function build() {
         cat $BUILD_DIR/FreeImage/Source/LibJXR/image/decode/segdec.c >> $BUILD_DIR/FreeImagePatched/Source/LibJXR/image/decode/segdec.c
         echo "#include <wchar.h>" > $BUILD_DIR/FreeImagePatched/Source/LibJXR/jxrgluelib/JXRGlueJxr.c
         cat $BUILD_DIR/FreeImage/Source/LibJXR/jxrgluelib/JXRGlueJxr.c >> $BUILD_DIR/FreeImagePatched/Source/LibJXR/jxrgluelib/JXRGlueJxr.c
+        sed -i "s/CXXFLAGS ?=/CXXFLAGS ?= -std=c++11/g" $BUILD_DIR/FreeImagePatched/Makefile.gnu
         cd $BUILD_DIR/FreeImagePatched
         emmake make clean -f Makefile.gnu
         emmake make -j${PARALLEL_MAKE} -f Makefile.gnu libfreeimage.a
