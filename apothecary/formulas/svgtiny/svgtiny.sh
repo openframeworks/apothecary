87506531ec35d3d6eeeab2cf5f161f38e3b0b97b
ofTheo <theo@openframeworks.cc>
2022-11-11 20:08:38 -0800

    svgtiny fix closes #247

--- apothecary/formulas/svgtiny/svgtiny.sh
+++ apothecary/formulas/svgtiny/svgtiny.sh
@@ -72,11 +72,10 @@ function build() {
             export CFLAGS=-I$SYSROOT/usr/include
         fi
         
+        export CFLAGS="-I$LIBS_DIR/libxml2/include"
+
+        #incase we need pkg-config
         export CFLAGS="$(pkg-config libxml-2.0 --cflags)"
-        
-        if [ "$TYPE" == "linuxarmv6l" ] || [ "$TYPE" == "linuxarmv7l" ] ; then
-            export CFLAGS="-I$LIBS_DIR/libxml2/include"
-        fi
         
         make clean
 	    make -j${PARALLEL_MAKE}
