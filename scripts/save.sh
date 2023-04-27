46c5cef8f46bbb6d2adebe11839d18537990e3f3
Dan Rosser <danoli3@gmail.com>
2023-04-27 23:09:16 +1000

    Added Save / Load Functionality to save overbuilding when doing core update - will use local status.txt to store last build time for target platform and architecture

--- scripts/save.sh
+++ scripts/save.sh
@@ -8,9 +8,11 @@
 function save {
 
     # Check if the save file exists
-    SAVE_FILE="$SCRIPT_DIR/build_status.txt"
-    if [ ! -f ./$SAVE_FILE ]; then
-        touch ./$SAVE_FILE
+    #SAVE_FILE="$SCRIPT_DIR/build_status.txt"
+    LOCAL_SAVE_FILE="$6"
+    echo "save file: $SAVE_FILE 0:$0 1:$1 2:$2 3:$3 4:$4 5:$5 6:$6"
+    if [ ! -f $LOCAL_SAVE_FILE ]; then
+        touch $LOCAL_SAVE_FILE
     fi
 
     # Get input parameters
@@ -19,7 +21,7 @@ function save {
     ARCH="$3"
     BUILT="$4"
     VERSION="$5"
-    TAG="$6"
+    #TAG="$6"
     
     # Get current date and time
     BUILD_DATETIME=$(date +"%Y-%m-%d %H:%M:%S")
@@ -27,10 +29,10 @@ function save {
     # Create new or update existing entry in the text file
     # Only one unique entry for each device target, source target, and architecture
     # Format: DEVICE_TARGET|SOURCE_TARGET|ARCH|BUILT|VERSION|TAG|BUILD_DATETIME
-    if grep -q "$DEVICE_TARGET|$SOURCE_TARGET|$ARCH" "$SCRIPT_DIR/build_info.txt"; then
-        sed -i '' "s|$DEVICE_TARGET|$DEVICE_TARGET|$SOURCE_TARGET|$ARCH|.*|$DEVICE_TARGET|$SOURCE_TARGET|$ARCH|$BUILT|$VERSION|$TAG|$BUILD_DATETIME|" "SAVE_FILE"
+    if grep -q "$DEVICE_TARGET|$SOURCE_TARGET|$ARCH" "$LOCAL_SAVE_FILE"; then
+        sed -i '' "s/^$DEVICE_TARGET|$SOURCE_TARGET|$ARCH|\(.*\)$/$DEVICE_TARGET|$SOURCE_TARGET|$ARCH|$BUILT|$VERSION|$BUILD_DATETIME/" "$LOCAL_SAVE_FILE"
     else
-        echo "$DEVICE_TARGET|$SOURCE_TARGET|$ARCH|$BUILT|$VERSION|$TAG|$BUILD_DATETIME" >> "$SAVE_FILE"
+        echo "$DEVICE_TARGET|$SOURCE_TARGET|$ARCH|$BUILT|$VERSION|$BUILD_DATETIME" >> "${LOCAL_SAVE_FILE}"
     fi
 }
 
