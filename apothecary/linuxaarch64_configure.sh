4ec4c1e56661c6d4c872feb5cf801acdb9f28845
ofTheo <theo@openframeworks.cc>
2023-08-23 16:24:46 -0700

    shshs

--- apothecary/linuxaarch64_configure.sh
+++ apothecary/linuxaarch64_configure.sh
@@ -1,10 +1,10 @@
-export PKG_CONFIG_PATH=$SYSROOT/usr/lib/$TOOLCHAIN_PREFIX/pkgconfig:$SYSROOT/usr/share/pkgconfig:$SYSROOT/usr/lib/pkgconfig
+#export PKG_CONFIG_PATH=$SYSROOT/usr/lib/$TOOLCHAIN_PREFIX/pkgconfig:$SYSROOT/usr/share/pkgconfig:$SYSROOT/usr/lib/pkgconfig
 export CROSS_COMPILE=$TOOLCHAIN_ROOT/bin/$TOOLCHAIN_PREFIX-
 export CXX=${CROSS_COMPILE}g++
 export CC=${CROSS_COMPILE}gcc
 export AR=${CROSS_COMPILE}ar
 export LD=${CROSS_COMPILE}ld
 export RANLIB=${CROSS_COMPILE}ranlib
-export CFLAGS="--sysroot=${SYSROOT} -I$SYSROOT/usr/include -I$SYSROOT/opt/vc/include -I$SYSROOT/opt/vc/include/IL -I$SYSROOT/opt/vc/include/interface/vcos/pthreads -I$SYSROOT/opt/vc/include/interface/vmcs_host/linux -I$SYSROOT/opt/vc/lib -march=armv8-a -mfpu=vfp -mfloat-abi=hard -fPIC -ftree-vectorize -Wno-psabi -pipe -DSTANDALONE -DPIC -D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DTARGET_POSIX -DHAVE_LIBOPENMAX=2 -DOMX -DOMX_SKIP64BIT -DUSE_EXTERNAL_OMX -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM"
-export LDFLAGS="--sysroot=${SYSROOT} -L$SYSROOT/usr/lib -L$SYSROOT/usr/lib/arm-linux-gnueabihf -lm"
-export HOST=arm-linux-gnueabihf
+export CFLAGS="--sysroot=${SYSROOT} -I$SYSROOT/lib/gcc/aarch64-linux-gnu/10.3.1 -march=armv8-a -mfpu=vfp -mfloat-abi=hard -fPIC -ftree-vectorize -Wno-psabi -pipe -DSTANDALONE -DPIC -D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DTARGET_POSIX -DHAVE_LIBOPENMAX=2 -DOMX -DOMX_SKIP64BIT -DUSE_EXTERNAL_OMX -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM"
+export LDFLAGS="--sysroot=${SYSROOT} -L$SYSROOT/aarch64-linux-gnu/lib64 -lm"
+export HOST=aarch64-linux-gnu
