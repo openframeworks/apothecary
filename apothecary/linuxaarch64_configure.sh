c1d025ae88749c2d864f30b78cce2b4fc4d0116e
ofTheo <theo@openframeworks.cc>
2023-08-23 18:53:25 -0700

    linker

--- apothecary/linuxaarch64_configure.sh
+++ apothecary/linuxaarch64_configure.sh
@@ -6,7 +6,7 @@ export AR=${CROSS_COMPILE}ar
 export LD=${CROSS_COMPILE}ld
 export RANLIB=${CROSS_COMPILE}ranlib
 export CFLAGS="--sysroot=${SYSROOT} -I$SYSROOT/usr/include -I$SYSROOT/opt/vc/include -I$SYSROOT/opt/vc/include/IL -I$SYSROOT/opt/vc/include/interface/vcos/pthreads -I$SYSROOT/opt/vc/include/interface/vmcs_host/linux -I$SYSROOT/opt/vc/lib -march=armv8-a -fPIC -ftree-vectorize -Wno-psabi -pipe -DSTANDALONE -DPIC -D_REENTRANT -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS -DTARGET_POSIX -DHAVE_LIBOPENMAX=2 -DOMX -DOMX_SKIP64BIT -DUSE_EXTERNAL_OMX -DHAVE_LIBBCM_HOST -DUSE_EXTERNAL_LIBBCM_HOST -DUSE_VCHIQ_ARM"
-export LDFLAGS="--sysroot=${SYSROOT} -L$SYSROOT/usr/lib -L$SYSROOT/usr/lib/aarch64-linux-gnu -lm -march=armv8-a"
+export LDFLAGS="--sysroot=${SYSROOT} -L$SYSROOT/usr/lib -L$SYSROOT/usr/lib/aarch64-linux-gnu -L$TOOLCHAIN_ROOT/aarch64-linux-gnu/libc/lib64 -lm -march=armv8-a"
 export HOST=aarch64-linux-gnu
 
 GCCPATH="$TOOLCHAIN_ROOT/libexec/gcc/aarch64-linux-gnu/10.3.1"
