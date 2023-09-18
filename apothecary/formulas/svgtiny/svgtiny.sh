3cc1c1464a9e56c9b5d6f415d7863d60a1b4f21c
Dan Rosser <danoli3@gmail.com>
2023-09-19 00:14:29 +1000

    fix svgtiny libroot

--- apothecary/formulas/svgtiny/svgtiny.sh
+++ apothecary/formulas/svgtiny/svgtiny.sh
@@ -74,13 +74,13 @@ function prepare() {
 
 # executed inside the lib src dir
 function build() {
+	LIBS_ROOT=$(realpath $LIBS_DIR)
     if [ "$TYPE" == "linux" ] || [ "$TYPE" == "linux64" ] || [ "$TYPE" == "linuxaarch64" ] || [ "$TYPE" == "linuxarmv6l" ] || [ "$TYPE" == "linuxarmv7l" ] ; then
         if [ $CROSSCOMPILING -eq 1 ]; then
             source ../../${TYPE}_configure.sh
             export LDFLAGS=-L$SYSROOT/usr/lib
             export CFLAGS=-I$SYSROOT/usr/include
         fi
-        LIBS_ROOT=$(realpath $LIBS_DIR)
         LIBXML2_ROOT="$LIBS_ROOT/libxml2/"
         LIBXML2_INCLUDE_DIR="$LIBS_ROOT/libxml2/include"
         LIBXML2_LIBRARY="$LIBS_ROOT/libxml2/lib/$TYPE/libxml2.a"
@@ -107,7 +107,6 @@ function build() {
 	    cmake --build . --config Release
 	    cd ..
 	elif [ "$TYPE" == "vs" ] ; then
-		LIBS_ROOT=$(realpath $LIBS_DIR)
         LIBXML2_ROOT="$LIBS_ROOT/libxml2/"
         LIBXML2_INCLUDE_DIR="$LIBS_ROOT/libxml2/include"
         LIBXML2_LIBRARY="$LIBS_ROOT/libxml2/lib/$TYPE/$PLATFORM/libxml2.lib"
@@ -148,7 +147,6 @@ function build() {
             export LDFLAGS=-L$SYSROOT/usr/lib
             export CFLAGS=-I$SYSROOT/usr/include
         fi
-		LIBS_ROOT=$(realpath $LIBS_DIR)
         LIBXML2_ROOT="$LIBS_ROOT/libxml2/"
         LIBXML2_INCLUDE_DIR="$LIBS_ROOT/libxml2/include"
         LIBXML2_LIBRARY="$LIBS_ROOT/libxml2/lib/$TYPE/$PLATFORM/libxml2.lib"
