6bcdea4adfb286bef20aed08df367421cc91ce87
Dan Rosser <danoli3@gmail.com>
2023-09-26 03:44:51 +1000

    fix toolchain

--- apothecary/aarch64-linux-gnu.toolchain.cmake
+++ apothecary/aarch64-linux-gnu.toolchain.cmake
@@ -1,16 +1,20 @@
+#
+# CMake Toolchain file for crosscompiling on ARM.
+#
+# Target operating system name.
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_C_COMPILER "aarch64-linux-gnu-gcc")
-set(CMAKE_CXX_COMPILER "aarch64-linux-gnu-g++")
+# Name of C compiler.
+set(CMAKE_C_COMPILER "/usr/bin/aarch64-linux-gnu-gcc-8")
+set(CMAKE_CXX_COMPILER "/usr/bin/aarch64-linux-gnu-g++-8")
 
+# Where to look for the target environment. (More paths can be added here)
+set(CMAKE_FIND_ROOT_PATH /usr/aarch64-linux-gnu)
+#set(CMAKE_SYSROOT /usr/aarch64-linux-gnu)
+
+# Adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment only.
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(CMAKE_C_FLAGS "-march=armv8-a")
-set(CMAKE_CXX_FLAGS "-march=armv8-a")
-
-# cache flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE arm64)
