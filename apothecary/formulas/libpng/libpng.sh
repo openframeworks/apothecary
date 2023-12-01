5b2f6167988ba8ac3e4e89824c1c035e825c5309
Dan Rosser <danoli3@gmail.com>
2023-12-02 05:57:53 +1100

    macOS fix for libpng

--- apothecary/formulas/libpng/libpng.sh
+++ apothecary/formulas/libpng/libpng.sh
@@ -164,7 +164,12 @@ function copy() {
 		mkdir -p $1/include
 		cp -v "build_${TYPE}_${ARCH}/Release/lib/libpng16_static.lib" $1/lib/$TYPE/$PLATFORM/libpng.lib
 		cp -RvT "build_${TYPE}_${ARCH}/Release/include/" $1/include
-
+  elif [ "$TYPE" == "osx" ] ; then
+		mkdir -p $1/lib/$TYPE/$PLATFORM/
+		mkdir -p $1/include
+		cp -v "build_${TYPE}_${PLATFORM}/Release/lib/libpng16.a" $1/lib/$TYPE/$PLATFORM/libpng16.a
+		cp -v "build_${TYPE}_${PLATFORM}/Release/lib/libpng.a" $1/lib/$TYPE/$PLATFORM/libpng.a
+		cp -Rv "build_${TYPE}_${PLATFORM}/Release/include/" $1/include
 	else
 		mkdir -p $1/include
 		mkdir -p $1/lib/$TYPE/$PLATFORM/
