2920ec277a5a9da33aab788d993266fc6cb96a7e
Dan Rosser <danoli3@gmail.com>
2024-02-18 11:52:02 +1100

    Merge

--- apothecary/formulas/libpng/libpng.sh
+++ apothecary/formulas/libpng/libpng.sh
@@ -16,7 +16,7 @@ URL=https://github.com/pnggroup/libpng/archive/refs/tags/v${VER}
 SHA=
 WINDOWS_URL=https://github.com/pnggroup/libpng/archive/refs/tags/v${VER}
 
-FORMULA_TYPES=( "osx" "vs" "ios" "watchos" "catos" "xros" "tvos" )
+FORMULA_TYPES=( "osx" "vs" "ios" "watchos" "catos" "xros" "tvos" "android" "emscripten" )
 
 FORMULA_DEPENDS=( "zlib" ) 
 
@@ -68,37 +68,39 @@ function prepare() {
 # executed inside the lib src dir
 function build() {
 	LIBS_ROOT=$(realpath $LIBS_DIR)
-	
-	if [[ "$TYPE" =~ ^(osx|ios|tvos|xros|catos|watchos)$ ]]; then
-		mkdir -p "build_${TYPE}_${PLATFORM}"
-		cd "build_${TYPE}_${PLATFORM}"
 
-		ZLIB_ROOT="$LIBS_ROOT/zlib/"
-		ZLIB_INCLUDE_DIR="$LIBS_ROOT/zlib/include"
-		ZLIB_LIBRARY="$LIBS_ROOT/zlib/lib/$TYPE/$PLATFORM/zlib.a"
-
-		DEFS="-DCMAKE_BUILD_TYPE=Release \
+	DEFS="
 		    -DCMAKE_C_STANDARD=17 \
 		    -DCMAKE_CXX_STANDARD=17 \
 		    -DCMAKE_CXX_STANDARD_REQUIRED=ON \
 		    -DCMAKE_CXX_EXTENSIONS=OFF \
 		    -DCMAKE_PREFIX_PATH=${LIBS_ROOT} \
-		    -DZLIB_ROOT=${ZLIB_ROOT} \
-		    -DZLIB_LIBRARY=${ZLIB_INCLUDE_DIR} \
-		    -DZLIB_INCLUDE_DIRS=${ZLIB_LIBRARY} \
 		    -DPNG_BUILD_ZLIB=OFF \
 		    -DPNG_TESTS=OFF \
 		    -DPNG_SHARED=OFF \
 		    -DPNG_STATIC=ON \
 		    -DBUILD_SHARED_LIBS=ON \
 		    -DPNG_HARDWARE_OPTIMIZATIONS=ON \
-				-DCMAKE_INSTALL_PREFIX=Release \
-				-DCMAKE_INCLUDE_OUTPUT_DIRECTORY=include \
-				-DCMAKE_INSTALL_INCLUDEDIR=include"
+			-DCMAKE_INCLUDE_OUTPUT_DIRECTORY=include \
+			-DCMAKE_INSTALL_INCLUDEDIR=include"
+	
+	if [[ "$TYPE" =~ ^(osx|ios|tvos|xros|catos|watchos)$ ]]; then
+		mkdir -p "build_${TYPE}_${PLATFORM}"
+		cd "build_${TYPE}_${PLATFORM}"
+		rm -f CMakeCache.txt *.a *.o
 
-			cmake .. ${DEFS} \
+		ZLIB_ROOT="$LIBS_ROOT/zlib/"
+		ZLIB_INCLUDE_DIR="$LIBS_ROOT/zlib/include"
+		ZLIB_LIBRARY="$LIBS_ROOT/zlib/lib/$TYPE/$PLATFORM/zlib.a"		
+
+		cmake .. ${DEFS} \
 				-DCMAKE_TOOLCHAIN_FILE=$APOTHECARY_DIR/toolchains/ios.toolchain.cmake \
 				-DPLATFORM=$PLATFORM \
+				-DZLIB_ROOT=${ZLIB_ROOT} \
+		    	-DZLIB_LIBRARY=${ZLIB_LIBRARY} \
+		    	-DZLIB_INCLUDE_DIRS=${ZLIB_INCLUDE_DIR} \
+				-DCMAKE_INSTALL_PREFIX=Release \
+				-DCMAKE_BUILD_TYPE=Release \
 				-DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
 				-DCMAKE_C_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
 				-DENABLE_BITCODE=OFF \
@@ -110,51 +112,108 @@ function build() {
 		cd ..	
 	elif [ "$TYPE" == "vs" ] ; then
 		echoVerbose "building $TYPE | $ARCH | $VS_VER | vs: $VS_VER_GEN"
-	  echoVerbose "--------------------"
-	  GENERATOR_NAME="Visual Studio ${VS_VER_GEN}" 
+	  	echoVerbose "--------------------"
+	  	GENERATOR_NAME="Visual Studio ${VS_VER_GEN}" 
 
-	  mkdir -p "build_${TYPE}_${ARCH}"
+	  	mkdir -p "build_${TYPE}_${ARCH}"
 		cd "build_${TYPE}_${ARCH}"
+		rm -f CMakeCache.txt *.lib *.o
 
-		Z_ROOT="$LIBS_ROOT/zlib/"
-		Z_INCLUDE_DIR="$LIBS_ROOT/zlib/include"
-		Z_LIBRARY="$LIBS_ROOT/zlib/lib/$TYPE/$PLATFORM/zlib.lib"
+		ZLIB_ROOT="$LIBS_ROOT/zlib/"
+		ZLIB_INCLUDE_DIR="$LIBS_ROOT/zlib/include"
+		ZLIB_LIBRARY="$LIBS_ROOT/zlib/lib/$TYPE/$PLATFORM/zlib.lib"
 
-		DEFS="
-				-DCMAKE_BUILD_TYPE=Release \
-				-DCMAKE_C_STANDARD=17 \
-				-DCMAKE_CXX_STANDARD=17 \
-				-DCMAKE_CXX_STANDARD_REQUIRED=ON \
-				-DCMAKE_CXX_EXTENSIONS=OFF \
-				-DZLIB_ROOT=${Z_ROOT} \
-				-DZLIB_LIBRARY=${Z_LIBRARY} \
-				-DZLIB_INCLUDE=${Z_INCLUDE_DIR} \
-				-DPNG_TESTS=OFF \
-				-DPNG_SHARED=OFF \
-				-DPNG_STATIC=ON \
-				-DBUILD_SHARED_LIBS=ON \
-				-DCMAKE_INSTALL_PREFIX=Release \
-				-DCMAKE_INCLUDE_OUTPUT_DIRECTORY=include \
-				-DCMAKE_INSTALL_INCLUDEDIR=include \
-				-DCMAKE_INSTALL_LIBDIR=lib"
-
-  env CXXFLAGS="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${CALLING_CONVENTION}"
-  env CFLAGS="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${CALLING_CONVENTION}"
-	cmake .. ${DEFS} \
+  		env CXXFLAGS="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${CALLING_CONVENTION}"
+  		env CFLAGS="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${CALLING_CONVENTION}"
+		cmake .. ${DEFS} \
 			-B . \
-	    -A "${PLATFORM}" \
-	    -G "${GENERATOR_NAME}" \
-	    ${CMAKE_WIN_SDK} \
-	    -UCMAKE_CXX_FLAGS \
-	    -UCMAKE_C_FLAGS \
-	    -DCMAKE_CXX_FLAGS_RELEASE="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${EXCEPTION_FLAGS}" \
-      -DCMAKE_C_FLAGS_RELEASE="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${EXCEPTION_FLAGS}" \
-	    -DCMAKE_VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE} \
-	    -D BUILD_SHARED_LIBS=ON
+			-DZLIB_ROOT=${ZLIB_ROOT} \
+	    	-DZLIB_LIBRARY=${ZLIB_LIBRARY} \
+	    	-DZLIB_INCLUDE_DIRS=${ZLIB_INCLUDE_DIR} \
+	    	-DCMAKE_INSTALL_PREFIX=Release \
+			-DCMAKE_BUILD_TYPE=Release \
+		    -A "${PLATFORM}" \
+		    -G "${GENERATOR_NAME}" \
+		    ${CMAKE_WIN_SDK} \
+		    -UCMAKE_CXX_FLAGS \
+		    -UCMAKE_C_FLAGS \
+		    -DCMAKE_CXX_FLAGS_RELEASE="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${EXCEPTION_FLAGS}" \
+	      	-DCMAKE_C_FLAGS_RELEASE="-DUSE_PTHREADS=1 ${VS_C_FLAGS} ${FLAGS_RELEASE} ${EXCEPTION_FLAGS}" \
+		    -DCMAKE_VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE} \
+		    -D BUILD_SHARED_LIBS=ON
 
-	cmake --build . --config Release  --target install
+		cmake --build . --config Release  --target install
 
-	cd ..	
+		cd ..	
+
+	if [ "$TYPE" == "android" ] ; then
+
+		source ../../android_configure.sh $ABI cmake
+
+		mkdir -p "build_${TYPE}_${PLATFORM}"
+		cd "build_${TYPE}_${PLATFORM}"
+		rm -f CMakeCache.txt *.a *.o
+
+		export CFLAGS="$CFLAGS $EXTRA_LINK_FLAGS -DNDEBUG -ffast-math -DPNG_ARM_NEON_OPT=0 -DDISABLE_PERF_MEASUREMENT -frtti -std=c17"
+		export CXXFLAGS="$CFLAGS $EXTRA_LINK_FLAGS -DNDEBUG -ffast-math -DPNG_ARM_NEON_OPT=0 -DDISABLE_PERF_MEASUREMENT -frtti -std=c++17"
+		export LDFLAGS="$LDFLAGS $EXTRA_LINK_FLAGS -shared"
+
+
+		ZLIB_ROOT="$LIBS_ROOT/zlib/"
+		ZLIB_INCLUDE_DIR="$LIBS_ROOT/zlib/include"
+		ZLIB_LIBRARY="$LIBS_ROOT/zlib/lib/$TYPE/$ABI/zlib.a"	
+
+			cmake .. ${DEFS} \
+				-DCMAKE_TOOLCHAIN_FILE=${NDK_ROOT}/build/cmake/android.toolchain.cmake \
+				-DPLATFORM=$PLATFORM \
+				-DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
+				-DCMAKE_C_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE}" \
+				D CMAKE_C_COMPILER=${CC} \
+				-DCMAKE_INSTALL_PREFIX=Release \
+				-DCMAKE_BUILD_TYPE=Release \
+	     	 	-D CMAKE_CXX_COMPILER_RANLIB=${RANLIB} \
+	     	 	-D CMAKE_C_COMPILER_RANLIB=${RANLIB} \
+	     	 	-D CMAKE_CXX_COMPILER_AR=${AR} \
+	     	 	-D CMAKE_C_COMPILER_AR=${AR} \
+	     	 	-D CMAKE_C_COMPILER=${CC} \
+	     	 	-D CMAKE_CXX_COMPILER=${CXX} \
+	     	 	-D CMAKE_C_FLAGS=${CFLAGS} \
+	     	 	-D CMAKE_CXX_FLAGS=${CXXFLAGS} \
+	        	-D ANDROID_ABI=${ABI} \
+	        	-D CMAKE_CXX_STANDARD_LIBRARIES=${LIBS} \
+	        	-D CMAKE_C_STANDARD_LIBRARIES=${LIBS} \
+	        	-D CMAKE_STATIC_LINKER_FLAGS=${LDFLAGS} \
+	        	-D ANDROID_NATIVE_API_LEVEL=${ANDROID_API} \
+	        	-D ANDROID_TOOLCHAIN=clang \
+				-DENABLE_VISIBILITY=OFF \
+				-DCMAKE_VERBOSE_MAKEFILE=${VERBOSE_MAKEFILE} \
+				-DCMAKE_POSITION_INDEPENDENT_CODE=TRUE
+		cmake --build . --config Release --target install
+		cd ..
+	elif [ "$TYPE" == "emscripten" ]; then
+		mkdir -p build_$TYPE
+	    cd build_$TYPE
+	    rm -f CMakeCache.txt *.a *.o
+
+	    ZLIB_ROOT="$LIBS_ROOT/zlib/"
+		ZLIB_INCLUDE_DIR="$LIBS_ROOT/zlib/include"
+		ZLIB_LIBRARY="$LIBS_ROOT/zlib/lib/$TYPE/$PLATFORM/zlib.a"
+
+	    $EMSDK/upstream/emscripten/emcmake cmake .. \
+	    	-B build \
+			-DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1" \
+			-DCMAKE_C_FLAGS="-DUSE_PTHREADS=1" \
+			-DCMAKE_CXX_EXTENSIONS=OFF \
+			-DCMAKE_INSTALL_PREFIX=Release \
+			-DCMAKE_BUILD_TYPE=Release \
+			-DBUILD_SHARED_LIBS=OFF \
+			-DDPNG_BUILD_ZLIB=OFF \
+			-DZLIB_ROOT=${ZLIB_ROOT} \
+		    -DZLIB_LIBRARY=${ZLIB_LIBRARY} \
+		    -DZLIB_INCLUDE_DIRS=${ZLIB_INCLUDE_DIR}
+	    cmake --build build --target install --config Release
+	    cd ..
+	fi
 		
 	fi
 
@@ -172,8 +231,17 @@ function copy() {
 		mkdir -p $1/lib/$TYPE/$PLATFORM/
 		mkdir -p $1/include
 		cp -v "build_${TYPE}_${PLATFORM}/Release/lib/libpng16.a" $1/lib/$TYPE/$PLATFORM/libpng.a
-		# cp -v "build_${TYPE}_${PLATFORM}/Release/lib/libpng.a" $1/lib/$TYPE/$PLATFORM/libpng.a
 		cp -R "build_${TYPE}_${PLATFORM}/Release/include/" $1/include
+	elif [ "$TYPE" == "android" ] ; then
+		mkdir -p $1/lib/$TYPE/$PLATFORM/
+		mkdir -p $1/include
+		cp -v "build_${TYPE}_${ABI}/Release/lib/libpng16_static.lib" $1/lib/$TYPE/$ABI/libpng.a
+		cp -RT "build_${TYPE}_${ABI}/Release/include/" $1/include
+	elif [ "$TYPE" == "emscripten" ] ; then
+		mkdir -p $1/lib/$TYPE/$PLATFORM/
+		mkdir -p $1/include
+		cp -v "build_${TYPE}/Release/lib/libpng16_static.wasm" $1/lib/$TYPE/libpng.wasm
+		cp -RT "build_${TYPE}/Release/include/" $1/include
 	else
 		mkdir -p $1/include
 		mkdir -p $1/lib/$TYPE/$PLATFORM/
