5e12d2c775b4344cee2f7852ca3f17267a310c15
Dan Rosser <danoli3@gmail.com>
2024-06-07 22:39:05 +1000

    Script optimisation, consolidation

--- scripts/package.sh
+++ scripts/package.sh
@@ -259,7 +259,9 @@ if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]] || [[
     echo "On Master or Bleeding Branch and not a PR - zipping build";
 else
     echo "This is a PR or not master/bleeding branch, exiting build before compressing";
-    # exit 0
+    if [ -z "${RELEASE+x}" ]; then
+        exit 0
+    fi
 fi
 
 if [ -z ${APPVEYOR+x} ]; then
