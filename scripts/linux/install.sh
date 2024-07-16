0d02512dae3d4c17d603d6c48d3734a680a51f8a
Dan Rosser <danoli3@gmail.com>
2024-07-16 23:08:55 +1000

    gcc linux oh you at 14

--- scripts/linux/install.sh
+++ scripts/linux/install.sh
@@ -97,17 +97,47 @@ elif [ "$OPT" == "gcc7" ]; then
     g++ -v
 elif [ "$OPT" == "gcc11" ]; then
     # https://gcc.gnu.org/gcc-11/changes.html
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
-    sudo apt-get update
-    sudo add-apt-repository -y 'deb http://mirrors.kernel.org/ubuntu hirsute main universe'
-    sudo apt-get update
-    sudo apt-get install -y --allow-unauthenticated gcc-11 g++-11
-    sudo apt-get install -y gperf coreutils libxrandr-dev libxinerama-dev libx11-dev libxcursor-dev libxi-dev
-    sudo apt-get autoremove
-    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 100
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100
-    sudo add-apt-repository -y 'deb http://mirrors.kernel.org/ubuntu hirsute main universe'
-    which gcc-11
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt update
+    sudo apt install gcc-11 gcc-12 g++-12 gcc-13 g++-13 -y
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 11 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+    sudo update-alternatives --config gcc
+    gcc --version
+    g++ -v
+elif [ "$OPT" == "gcc12" ]; then
+    # https://gcc.gnu.org/gcc-12/changes.html
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt update
+    sudo apt install gcc-12 g++-12 -y
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 12 --slave /usr/bin/g++ g++ /usr/bin/g++-12
+    sudo update-alternatives --config gcc
+    gcc --version
+    g++ -v
+elif [ "$OPT" == "gcc13" ]; then
+    # https://gcc.gnu.org/gcc-13/changes.html
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt update
+    sudo apt install gcc-13 g++-13 -y
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 13 --slave /usr/bin/g++ g++ /usr/bin/g++-13
+    sudo update-alternatives --config gcc
+    gcc --version
+    g++ -v
+elif [ "$OPT" == "gcc14" ]; then
+    # https://gcc.gnu.org/gcc-14/changes.html
+    sudo apt update
+    sudo apt install software-properties-common
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt update
+    sudo apt install gcc-13 g++-13 -y
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-14 14 --slave /usr/bin/g++ g++ /usr/bin/g++-14
+    sudo update-alternatives --config gcc
+    gcc --version
     g++ -v
 else
 	echo "GCC version not specified on OPT env var, set one of gcc4, gcc5 or gcc6"
