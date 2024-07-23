bf16980e029927150512f113479ee04de8750476
Dan Rosser <danoli3@gmail.com>
2024-07-23 18:00:12 +1000

    zlib fix merge

--- apothecary/formulas/zlib/zlib.sh
+++ apothecary/formulas/zlib/zlib.sh
@@ -228,23 +228,10 @@ function copy() {
 		export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH}:$1/lib/$TYPE/$PLATFORM"
 
     elif [ "$TYPE" == "linux" ] || [ "$TYPE" == "linux64" ] || [ "$TYPE" == "linuxaarch64" ] || [ "$TYPE" == "linuxarmv6l" ] || [ "$TYPE" == "linuxarmv7l" ] || [ "$TYPE" == "msys2" ]; then
-<<<<<<< HEAD
-=======
 		mkdir -p $1/include    
 	    mkdir -p $1/lib/$TYPE/$PLATFORM
->>>>>>> 63674b5 (zlib target folder)
 		cp -Rv "build_${TYPE}_${ARCH}/Release/include/"* $1/include/ > /dev/null 2>&1
-		mkdir -p $1/lib/$TYPE/$ARCH/
-<<<<<<< HEAD
-<<<<<<< HEAD
-        cp -v "build_${TYPE}_${ARCH}/Release/z.a" $1/lib/$TYPE/$PLATFORM/zlib.a > /dev/null 2>&1
-=======
-        cp -v "build_${TYPE}_${ARCH}/Release/libz.a" $1/lib/$TYPE/$PLATFORM/zlib.a > /dev/null 2>&1
-=======
         cp -v "build_${TYPE}_${ARCH}/Release/lib/libz.a" $1/lib/$TYPE/$PLATFORM/zlib.a > /dev/null 2>&1
->>>>>>> 12f3df4 (release path zlib)
-        . "$SECURE_SCRIPT"
->>>>>>> 3f69c09 (zlib location)
         secure $1/lib/$TYPE/$PLATFORM/zlib.a
 	else
 		make install
