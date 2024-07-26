47b6a783670470d019b5478dc74331611d6e24b4
Dan Rosser <danoli3@gmail.com>
2024-07-27 02:41:50 +1000

    FreeImage Unicode fix [3.19.5 -> 3.19.6]

--- apothecary/formulas/FreeImage/FreeImage.sh
+++ apothecary/formulas/FreeImage/FreeImage.sh
@@ -13,9 +13,9 @@ FORMULA_TYPES=( "osx" "vs" "ios" "watchos" "catos" "xros" "tvos" "android" "emsc
 
 FORMULA_DEPENDS=( "zlib" "libpng" )
 
-VER=31950
+VER=31960
 GIT_URL=https://github.com/danoli3/FreeImage
-GIT_TAG=3.19.5
+GIT_TAG=3.19.6
 
 # download the source code and unpack it into LIB_NAME
 function download() {
