1b655a7aabe68c6ecf3fc27766aa6daecde66f62
Dan Rosser <danoli3@gmail.com>
2024-07-27 03:03:53 +1000

    Actions Unicode. Rebuild all

--- apothecary/formulas/FreeImage/FreeImage.sh
+++ apothecary/formulas/FreeImage/FreeImage.sh
@@ -13,9 +13,9 @@ FORMULA_TYPES=( "osx" "vs" "ios" "watchos" "catos" "xros" "tvos" "android" "emsc
 
 FORMULA_DEPENDS=( "zlib" "libpng" )
 
-VER=31960
+VER=31970
 GIT_URL=https://github.com/danoli3/FreeImage
-GIT_TAG=3.19.6
+GIT_TAG=3.19.7
 
 # download the source code and unpack it into LIB_NAME
 function download() {
