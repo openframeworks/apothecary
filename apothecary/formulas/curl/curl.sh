8e4adb18893e1fc5c61c78dc620d64a012848317
Dan Rosser <danoli3@gmail.com>
2024-11-13 01:38:17 +1100

    cURL HAVE_GETPASS_R=0

--- apothecary/formulas/curl/curl.sh
+++ apothecary/formulas/curl/curl.sh
@@ -303,6 +303,7 @@ function build() {
             -DCMAKE_CXX_STANDARD_REQUIRED=ON \
             -DCMAKE_CXX_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE} -Wno-error=implicit-function-declaration" \
             -DCMAKE_C_FLAGS="-DUSE_PTHREADS=1 ${FLAG_RELEASE} -Wno-error=implicit-function-declaration" \
+            -DHAVE_GETPASS_R=0 \
             -DCURL_DISABLE_LIBSSH2=ON \
             -DCURL_DISABLE_LIBPSL=ON \
             -DCMAKE_CXX_EXTENSIONS=OFF \
