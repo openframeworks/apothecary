bf5f4a9a42dc70d03d149bf9536a4db8a1d947c0
Dan Rosser <danoli3@gmail.com>
2025-01-15 04:05:50 +1100

    Install Linux

--- scripts/linux/arm64/install.sh
+++ scripts/linux/arm64/install.sh
@@ -121,12 +121,12 @@ sudo apt-get -y install libasound-dev libjack-dev libpulse-dev oss4-dev #rtaudio
 
 sudo apt-get update && sudo apt-get install -y autoconf libtool automake dos2unix
 sudo apt-get update && sudo apt-get install -y cmake build-essential
-sudo apt-get update && sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev
+sudo apt-get update && sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev cmake build-essential libc6-dev
 
 sudo apt-get install -y aptitude build-essential gawk gcc g++ gfortran git texinfo bison libncurses-dev cmake unzip pkg-config flex openssl pigz autoconf automake tar figlet xz-utils libtool dos2unix
 sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libxrandr-dev libxinerama-dev libx11-dev libxext-dev libxcursor-dev libxi-dev ccache
 sudo aptitude install -y gperf
-
+\
 dpkg -L gcc-aarch64-linux-gnu
 
 # Download the installer script
--- scripts/linux/armv6l/install.sh
+++ scripts/linux/armv6l/install.sh
@@ -8,3 +8,5 @@ APOTHECARY_LEVEL="$( cd "$SCRIPT_DIR/../.." && pwd )"
 
 sudo apt-get install -y aptitude build-essential gawk gcc g++ gfortran git texinfo bison libncurses-dev cmake unzip pkg-config flex openssl pigz autoconf automake tar figlet xz-utils
 sudo aptitude install -y gperf
+sudo apt-get update && sudo apt-get install -y libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libxrandr-dev libxinerama-dev libx11-dev libxext-dev libxcursor-dev libxi-dev
+sudo apt-get install -y ccache
