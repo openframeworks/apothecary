a53a2f604a43e4817724b4300ad18db6ee828665
Dan Rosser <danoli3@gmail.com>
2025-01-15 03:05:08 +1100

    Apothecary Fixes for echos / zlib

--- apothecary/formulas/zlib/zlib.sh
+++ apothecary/formulas/zlib/zlib.sh
@@ -105,8 +105,8 @@ function build() {
 		mkdir -p "build_${TYPE}_${ABI}"
 		cd "build_${TYPE}_${ABI}"
 		rm -f CMakeCache.txt *.a *.o
-		export CFLAGS="$CFLAGS $FLAGS_RELEASE -DNDEBUG -std=${CPP_STANDARD}"
-		export CXXFLAGS="$CFLAGS $FLAGS_RELEASE -DNDEBUG -std=${C_STANDARD}"
+		export CFLAGS="$CFLAGS $FLAG_RELEASE -DNDEBUG -std=${CPP_STANDARD}"
+		export CXXFLAGS="$CFLAGS $FLAG_RELEASE -DNDEBUG -std=${C_STANDARD}"
 
 		cmake .. ${DEFS} \
 				-DCMAKE_TOOLCHAIN_FILE=${NDK_ROOT}/build/cmake/android.toolchain.cmake \
