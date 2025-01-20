2cedacd8f0acbbff150f1bd08dabb24b70caf503
Dan Rosser <danoli3@gmail.com>
2025-01-20 23:31:53 +1100

    debug paths android toolchain

--- apothecary/configure/android_configure.sh
+++ apothecary/configure/android_configure.sh
@@ -64,11 +64,14 @@ export TOOLCHAIN_PATH="${NDK_ROOT}/toolchains/llvm/prebuilt/${HOST_PLATFORM}/bin
 # export AR="$TOOLCHAIN_PATH/llvm-ar"
 # export CC="$TOOLCHAIN_PATH/${ANDROID_PREFIX}${ANDROID_API}-clang"
 # export CXX="$TOOLCHAIN_PATH/${ANDROID_PREFIX}${ANDROID_API}-clang++"
-# export LDFLAGS="-pie -L${SYSROOT}/usr/lib/$ANDROID_PREFIX/$ANDROID_API"
+# export LDFLAGS="-pie -L${SYSROOT}/usr/lib/$ANDROID_PREFIX/$ANDROID_API
+echo "debug paths"
+echo "${NDK_ROOT}/toolchains/llvm/prebuilt/"
 ls -a ${NDK_ROOT}/toolchains/llvm/prebuilt/
+echo "${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64/"
 ls -a ${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64/
-ls -a ${NDK_ROOT}/toolchains/llvm/
-ls ${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android-clang
+echo "${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64/bin"
+ls -a${NDK_ROOT}/toolchains/llvm/prebuilt/linux-x86_64/bin/
 
 # Debug output
 echo -e "\n\033[1;32m==== Android Toolchain Configuration ====\033[0m"
