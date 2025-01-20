5cae2b7b139a6359b7f48d94257ccb588ec8568b
Dan Rosser <danoli3@gmail.com>
2025-01-20 19:11:16 +1100

    os type update

--- apothecary/ostype.sh
+++ apothecary/ostype.sh
@@ -15,17 +15,51 @@ elif [ "${OS:0:5}" == "mingw" -o "$OS" == "msys_nt-6.3" ]; then
     OS="msys2"
 elif [ "$OS" == "linux" ]; then
     ARCH=$(uname -m)
-    if [ "$ARCH" == "i386" -o "$ARCH" == "i686" ]; then
-        OS="linux"
-    elif [ "$ARCH" == "x86_64" ]; then
-        OS="linux64"
-    elif [ "$ARCH" == "armv6l" ]; then
-        OS="linuxarmv6l"
-    elif [ "$ARCH" == "armv7l" ]; then
-        OS="linuxarmv7l"
+    if [[ -f /etc/os-release ]]; then
+        . /etc/os-release
+        case "$ID" in
+            ubuntu)
+                export DISTRO="ubuntu"
+                ;;
+            raspbian)
+                export DISTRO="raspios"
+                ;;
+            debian)
+                if grep -q "Raspberry Pi" /proc/cpuinfo; then
+                    export DISTRO="raspios"
+                else
+                    export DISTRO="debian"
+                fi
+                ;;
+            *)
+                export DISTRO="$ID"
+                ;;
+            echo "$ID"
+        esac
+    fi
+    if [[ "$DISTRO" == "raspios" ]]; then
+        if [[ -f /proc/cpuinfo ]]; then
+            REVISION=$(grep "^Revision" /proc/cpuinfo | awk '{print $3}')
+            case "$REVISION" in
+                    a02082|a22082|a32082|a52082)
+                        export DEVICE="Raspberry Pi 3"
+                        ;;
+                    a03111|b03111|b03112|b03114)
+                        export DEVICE="Raspberry Pi 4"
+                        ;;
+                    b03140|c03140|d03140|c03145)
+                        export DEVICE="Raspberry Pi 5"
+                        ;;
+                    *)
+                        export DEVICE="Unknown Raspberry Pi Model"
+                        ;;
+                esac
+            echo "$DEVICE"
+        else
+            export DEVICE="Unknown (no /proc/cpuinfo)"
+        fi
     else
-        # We don't know this one, but we will try to make a reasonable guess.
-        OS="linux"$ARCH
+        export DEVICE="Not a Raspberry Pi"
     fi
 fi
 
