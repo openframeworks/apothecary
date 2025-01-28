7af6fbdcd2e69c04bf292decddec536152ef6a6f
Dan Rosser <danoli3@gmail.com>
2025-01-28 15:27:55 +1100

    gstreamer

--- apothecary/formulas/gstreamer.sh
+++ apothecary/formulas/gstreamer.sh
@@ -88,9 +88,10 @@ function build() {
     --buildtype=release \
     --default-library=static \
     --backend=ninja \
-    -Dgst-full-libraries=app,video \
+    -Dgst-full-libraries=video \
     -Dc_args="-I${LIBPNG_INCLUDE_DIR} -I${ZLIB_INCLUDE_DIR} -I${FREETYPE_INCLUDE_DIR}" \
-    -Dc_link_args="${LIBPNG_LIBRARY} ${ZLIB_LIBRARY} ${FREETYPE_LIBRARY}"
+    -Dc_link_args="${LIBPNG_LIBRARY} ${ZLIB_LIBRARY} ${FREETYPE_LIBRARY} -lm" \
+    -Dcpp_link_args="${LIBPNG_LIBRARY} ${ZLIB_LIBRARY} ${FREETYPE_LIBRARY} -lm"
 
     ninja -C "$BUILD_DIR"
     ninja install -C "$BUILD_DIR"
