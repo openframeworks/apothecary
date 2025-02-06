3a2505a2e831b1a9328e4fd5728ce4487275d409
Dan Rosser <danoli3@gmail.com>
2025-02-06 23:44:47 +1100

    Apothecary changes to "core" to now use calculate formuales

--- scripts/calculate_formulas.sh
+++ scripts/calculate_formulas.sh
@@ -234,46 +234,9 @@ array_contains() {
     return $in
 }
 
-# If commit contains [build_only:formula1 formula2] only those formulas will be built
-# this will only work on a pull request, not when commiting to master
-if [[ ! -z "${APPVEYOR+x}" && "${APPVEYOR_REPO_NAME}" != "openframeworks/apothecary" ]]; then
-    echo ${APPVEYOR_REPO_NAME}
-fi
-if [[ ! -z "${TRAVIS_BRANCH+x}" && "$TRAVIS_BRANCH" != "master" && "$TRAVIS_PULL_REQUEST" != "false" ]] || [[ ! -z "${APPVEYOR+x}" && "${APPVEYOR_REPO_NAME}" != "openframeworks/apothecary" ]]; then
-    echo "DETECTED PULL REQUEST OR NOT MASTER BRANCH, CHECKING FILTERS"
-    COMMIT_MESSAGE="$(git log --no-decorate -n1 --no-merges)"
-    echo "COMMIT_MESSAGE $COMMIT_MESSAGE"
-    FORMULAS_FROM_COMMIT=($(echo $COMMIT_MESSAGE | sed -n "s/.*\[build_only:\([^]]*\)\]/\1/p" | sed "s/\[.*\]//g"))
-    PLATFORMS_FROM_COMMIT=($(echo $COMMIT_MESSAGE | sed -n "s/.*\[platforms_only:\([^]]*\)\]/\1/p" | sed "s/\[.*\]//g"))
-fi
-
-echo "FORMULAS_FROM_COMMIT: $FORMULAS_FROM_COMMIT"
-if [ ! -z "$FORMULAS_FROM_COMMIT" ]; then
-    FILTERED_FORMULAS=()
-    for formula in $FORMULAS_FROM_COMMIT; do
-        if [[ " ${FORMULAS[*]} " == *" $formula "* ]]; then
-            FILTERED_FORMULAS+=($formula)
-        fi
-        # array_contains $FORMULAS $formula && FILTERED_FORMULAS+=($formula)
-    done
-    echo "FILTERED_FORMULAS: $FILTERED_FORMULAS"
-    FORMULAS=(${FILTERED_FORMULAS})
-fi
-
 echo "FORMULAS: ${FORMULAS[@]}"
-
 if [ -z ${FORMULAS} ]; then
     echo "No formulas to build, failing"
     exit 1
 fi
 
-echo "PLATFORMS_FROM_COMMIT: $PLATFORMS_FROM_COMMIT"
-if [ ! -z "$PLATFORMS_FROM_COMMIT" ]; then
-    if [[ " ${PLATFORMS_FROM_COMMIT[*]} " == *" $TARGET "* ]]; then
-        echo "Platform $TARGET allowed in commit message"
-        exit 0
-    else
-        echo "Platform $TARGET NOT allowed in commit message"
-        exit 1
-    fi
-fi
