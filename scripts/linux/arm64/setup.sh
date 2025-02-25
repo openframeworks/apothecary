293a795437e42401317665874a276608c6c748ff
Dan Rosser <danoli3@gmail.com>
2025-02-25 21:04:04 +1100

    sysroot arm64

--- scripts/linux/arm64/setup.sh
+++ scripts/linux/arm64/setup.sh
@@ -210,8 +210,8 @@ dpkg -l | grep g++-aarch64-linux-gnu
 dpkg -L libx11-dev:arm64 | grep libX11.so
 dpkg -L libxext-dev:arm64 | grep libXext.so
 
-PKG_CONFIG_PATH=${SYSROOT}/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/share/pkgconfig:$PKG_CONFIG_PATH \
-PKG_CONFIG_LIBDIR=${SYSROOT}/usr/lib/aarch64-linux-gnu/pkgconfig \
-PKG_CONFIG_SYSROOT_DIR=${SYSROOT} \
+PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/share/pkgconfig:$PKG_CONFIG_PATH \
+PKG_CONFIG_LIBDIR=/usr/lib/aarch64-linux-gnu/pkgconfig \
+PKG_CONFIG_SYSROOT_DIR=/ \
 pkg-config --list-all
 
