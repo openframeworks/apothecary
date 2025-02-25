63014f7477e10e08d55a86a3c53b7a867e80ced3
Dan Rosser <danoli3@gmail.com>
2025-02-25 22:38:40 +1100

    msys2 clangarm64

--- scripts/msys2/install_msys2.sh
+++ scripts/msys2/install_msys2.sh
@@ -4,12 +4,14 @@
 # Update package database
 pacman -Syuu --noconfirm
 pacman -S --noconfirm base-devel unzip dos2unix git
-pacboy -S --noconfirm gcc cmake gperf libxml2 python3 zlib libpng fftw3 zlib
+pacboy -S --noconfirm gcc cmake gperf libxml2 python3 zlib libpng fftw3 zlib wget2 curl
 
 # Additional
 pacboy -S --noconfirm mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-ninja
 pacboy -S --noconfirm mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-openssl mingw-w64-x86_64-wxWidgets
 pacboy -S --noconfirm mingw-w64-x86_64-boost mingw-w64-x86_64-curl mingw-w64-x86_64-git
+
+pacboy -S --noconfirm mingw-w64-clang-aarch64-clang mingw-w64-clang-aarch64-toolchain mingw-w64-clang-aarch64-cmake mingw-w64-clang-aarch64-ninja
 
 # Clean
 pacman -Scc --noconfirm
