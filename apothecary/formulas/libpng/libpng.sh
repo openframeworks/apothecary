b8c0f878524666681572832d87582d7211f87a6c
Dan Rosser <danoli3@gmail.com>
2025-02-27 02:04:05 +1100

    copy depth

--- apothecary/formulas/libpng/libpng.sh
+++ apothecary/formulas/libpng/libpng.sh
@@ -351,8 +351,8 @@ function copy() {
     elif [ "$TYPE" == "emscripten" ]; then
         mkdir -p $1/lib/${TYPE}/${PLATFORM}/
         cp -v "build_${TYPE}_${PLATFORM}/Release/lib/libpng16.a" $1/lib/$TYPE/$PLATFORM/libpng16.a
-        cp -vR "build_${TYPE}_${PLATFORM}/Release/include/" $1/include
-        cp -vR "build_${TYPE}_${PLATFORM}/Release/include/" $1/include/libpng16
+        cp -vR build_${TYPE}_${PLATFORM}/Release/include/* $1/include/
+        cp -vR "build_${TYPE}_${PLATFORM}/Release/include/" $1/include/libpng16/
         # cp -vR "build_${TYPE}_${PLATFORM}/Release/lib/" $1/lib/${TYPE}/${PLATFORM}
         cp -vR "build_${TYPE}_${PLATFORM}/Release/lib/pkgconfig/libpng.pc" $1/lib/${TYPE}/${PLATFORM}/libpng.pc
         cp -vR "build_${TYPE}_${PLATFORM}/Release/lib/pkgconfig/libpng16.pc" $1/lib/${TYPE}/${PLATFORM}/libpng16.pc
